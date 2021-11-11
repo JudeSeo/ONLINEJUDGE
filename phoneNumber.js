d3720598581d7dcd74655c2d38023e487e582584
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2021-11-11 22:10:15 +0900

    Create phoneNumber.js
    프로그래머스 전화번호 가리기

---
+++ phoneNumber.js
@@ -0,0 +1,10 @@
+function solution(phone_number) {
+    var answer = '';
+    var leng = phone_number.length;
+    for(var i=4;i<leng;i++)
+        answer += "*";
+    answer += phone_number.substr(leng-4)
+    return answer;
+}
+// for문 없이 만드는 법이 필요할거 같다
+//없으면 점수 더 높을듯
