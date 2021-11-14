0e0a954b99f07c61a81562b55a4e16e9e9eda8d0
JudeSeo <gn0408@gmail.com>
2021-11-14 19:38:34 +0900

    프로그래머스 같은숫자는 싫어

---
+++ Programmers/P1_noSameNumbers.js
@@ -0,0 +1,14 @@
+// 2021-11-14
+function solution(arr)
+{
+    var answer = [];
+    for(var i=0; i<arr.length; i++){
+        if(arr[i] != arr[i+1])
+        answer.push(arr[i]);
+    }
+    console.log(answer);
+    return answer;
+}
+
+solution([1,1,3,3,0,1,1]);
+solution([4,4,4,3,3]);
