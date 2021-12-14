45f6bbcd0903a3f9e1492e604173fde86734790a
JudeSeo <gn0408@gmail.com>
2021-12-14 19:17:25 +0900

    프로그래머스 최소직사각형

---
+++ Programmers/P1_rectangle.js
@@ -0,0 +1,20 @@
+// 2021-12-14
+function solution(sizes) {
+    var answer = 0;
+    var small, big, width = 0 , height = 0;
+    for (var i = 0; i < sizes.length; i++) {
+        small = sizes[i][0] > sizes[i][1] ? sizes[i][1] : sizes[i][0]
+        big = sizes[i][0] < sizes[i][1] ? sizes[i][1] : sizes[i][0]
+        sizes[i][0] = small;
+        sizes[i][1] = big;
+        width = sizes[i][0] > width ? sizes[i][0] : width;
+        height = sizes[i][1] > height ? sizes[i][1] : height;
+    }
+    answer = width * height;
+    console.log(answer);
+    return answer;
+}
+
+solution([[60, 50], [30, 70], [60, 30], [80, 40]]);
+solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]);
+solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]);
