b2f339c7933f9c7b843c74b927dd13fb70431742
JudeSeo <gn0408@gmail.com>
2021-12-15 22:10:14 +0900

    프로그래머스 완주하지 못한 선수

---
+++ Programmers/P1_noGoalParti.js
@@ -0,0 +1,20 @@
+// 2012-12-15
+function solution(participant, completion) {
+    var answer = '';
+    participant.sort();
+    completion.sort();
+    for (var i = 0; i < participant.length; i++) {
+        if (participant[i] != completion[i]) {
+            answer = participant[i];
+            break;
+        }
+    }
+    console.log(answer);
+    return answer;
+}
+
+solution(["leo", "kiki", "eden"], ["eden", "kiki"]);
+solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]);
+solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]);
+
+//효율성 때문에 sort 한 후 비교해야함
