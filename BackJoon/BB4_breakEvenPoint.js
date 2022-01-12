50cd4147d397f2dad46c7779af48d4f0c8104638
JudeSeo <gn0408@gmail.com>
2022-01-12 22:34:13 +0900

    백준 손익분기점

---
+++ BackJoon/BB4_breakEvenPoint.js
@@ -0,0 +1,16 @@
+// 2022-01-12
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+var input = fs.readFileSync('예제.txt').toString().trim().split(' ');
+var first = Math.floor(input[0]);
+var n = 1;
+var benefit = Math.floor(input[2]) - Math.floor(input[1]);
+if (benefit <= 0)
+    n = -1;
+else
+    n = Math.floor(first / benefit) + 1;
+console.log(n)
+
+// 테스트는 잘 되서 제출했는데... 안되서 네이버 보고 했다....
+// parseInt 쓰면 음수에서 문제생겨서 Math.floor 쓰라는 네이버 글을 봤다..
+// 두가지의 구분이 필요하다
