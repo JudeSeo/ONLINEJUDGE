eb7253048c69041e064768f33203ed0d9d420c43
JudeSeo <gn0408@gmail.com>
2022-01-20 21:33:06 +0900

    백준 OX퀴즈

---
+++ BackJoon/BB2_OXQuiz.js
@@ -0,0 +1,21 @@
+// 2022-01-20
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let length = input[0];
+let answer = [];
+input = input.slice(1);
+input.forEach((v, i) => {
+    let score = 0;
+    let sum = 0;
+    for (var i = 0; i < v.length; i++) {
+        if (v.charAt(i) == 'O')
+            score++
+        else
+            score = 0;
+        sum += score;
+    }
+    answer.push(sum);
+});
+
+console.log(answer.join("\n"));
