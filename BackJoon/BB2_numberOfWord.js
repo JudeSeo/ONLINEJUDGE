977c22132d3ee5a9f544339021b1964da0f74374
JudeSeo <gn0408@gmail.com>
2022-01-22 21:30:57 +0900

    백준 단어의개수

---
+++ BackJoon/BB2_numberOfWord.js
@@ -0,0 +1,11 @@
+// 2022-01-22
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = input[0].trim().split(" ")
+if (answer[0] == "")
+    answer.length = 0;
+console.log(answer.length);
+
+// 쉬워보여서 걍 제출 했는데 틀려서 띠용...
+// 값이 아무것도 없을때 1로 측정되서 그거 예외처리 해주니까 통과
