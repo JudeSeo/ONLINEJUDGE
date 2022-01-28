66c715557500311221de3da977bdc38a07f19bf4
JudeSeo <gn0408@gmail.com>
2022-01-28 21:11:08 +0900

    백준 ??!

---
+++ BackJoon/BB5_questionMark.js
@@ -0,0 +1,5 @@
+// 2022-01-28
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+console.log(input[0]+"??!");
