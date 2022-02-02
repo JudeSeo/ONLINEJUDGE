4e3ef1d2c384c67b5e808254d009ff7c446d1404
JudeSeo <gn0408@gmail.com>
2022-02-02 22:10:56 +0900

    백준 셀프넘버

---
+++ BackJoon/BS5_selfNumber.js
@@ -0,0 +1,20 @@
+function solution(number) {
+    let arr = [];
+    for (let i = 1; i <= number; i++) {
+        let sum = i;
+        let num = i
+        while (num > 0) {
+            sum += num % 10;
+            num = parseInt(num / 10)
+        }
+        if (i != sum)
+            arr[sum] = 1;
+    }
+    for (let i = 1; i < number; i++) {
+        if (arr[i] != 1)
+            console.log(i);
+
+    }
+}
+
+solution(10000);
