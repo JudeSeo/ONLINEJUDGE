94d6906bf67f1aa555abfab597c38c922194afe9
JudeSeo <gn0408@gmail.com>
2022-02-03 22:54:37 +0900

    백준 나이순정렬

---
+++ BackJoon/BS5_ageSort.js
@@ -0,0 +1,9 @@
+// 2022-02-03
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+input = input.slice(1);
+input.sort((a, b) => {
+    return a.split(" ")[0] - b.split(" ")[0]
+})
+console.log(input.join("\n"));
