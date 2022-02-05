ae616a0ffabd9da34eafd08ebc78a618409d5e05
JudeSeo <gn0408@gmail.com>
2022-02-05 23:38:51 +0900

    프로그래머스 가장 큰 수

---
+++ Programmers/P2_biggestNum.js
@@ -0,0 +1,15 @@
+function solution(numbers) {
+    var answer = '';
+    numbers.sort((a, b) => {
+        return (b.toString() + a) - (a.toString() + b)
+    });
+
+    answer = numbers.join("")
+    console.log(answer);
+    return answer[0] == 0 ? '0' : answer;
+}
+solution([6, 10, 2]);
+solution([3, 30, 34, 5, 9]);
+
+
+// 다른 사람 풀이 참조해서 풀었다...
