b7465e2fd637f4f2e242efe33fda616af247f810
JudeSeo <gn0408@gmail.com>
2022-02-06 13:05:51 +0900

    백준 달팽이는 올라가고 싶다

---
+++ BackJoon/BB1_snail.js
@@ -0,0 +1,13 @@
+// 2022-02-03
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let up = parseInt(input[0].split(" ")[0]);
+let down = parseInt(input[0].split(" ")[1]);
+let dist = up - down
+let top = parseInt(input[0].split(" ")[2]);
+let answer = Math.ceil((top - down)/dist);
+console.log(answer);
+
+// 걍 while문 썻다가 시간초과...
+// 네이버 검색 후 수학적으로 풀었다..
