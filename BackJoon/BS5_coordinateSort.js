a23b6873c1c7d5ec59f8a8296d0a606697ee7a4d
JudeSeo <gn0408@gmail.com>
2022-02-14 19:36:28 +0900

    백준 좌표정렬하기

---
+++ BackJoon/BS5_coordinateSort.js
@@ -0,0 +1,14 @@
+// 2022-02-14
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+input = input.slice(1);
+input.sort((a, b) => {
+    if(a.split(" ")[0] != b.split(" ")[0]){
+        return a.split(" ")[0] - b.split(" ")[0]
+    } else {
+        return a.split(" ")[1] - b.split(" ")[1]
+    }
+})
+
+console.log(input.join("\n"));
