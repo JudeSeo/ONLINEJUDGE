06fba306776abdca5fa8d2b6f509809b5f139352
JudeSeo <gn0408@gmail.com>
2022-02-15 21:01:01 +0900

    백준 a+b-6

---
+++ BackJoon/BB2_a+b-6.js
@@ -0,0 +1,11 @@
+// 2022-02-15
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+input = input.slice(1);
+let answer = [];
+input.forEach((v, i)=>{
+    let temp = v.split(",");
+    answer.push(parseInt(temp[0])+parseInt(temp[1]))
+})
+console.log(answer.join("\n"));
