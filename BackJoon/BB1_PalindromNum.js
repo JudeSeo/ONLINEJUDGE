aee4cc876ad5e3fe1f3e02fb9fb92f3a99700bc6
JudeSeo <gn0408@gmail.com>
2022-02-15 21:03:15 +0900

    백준 팰린드롬수

---
+++ BackJoon/BB1_PalindromNum.js
@@ -0,0 +1,19 @@
+// 2022-02-15
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+input = input.slice(0,input.length-1)
+let answer = [];
+input.forEach((v, i)=>{
+    let temp = v.slice();
+    let cnt=0;
+    for(let i=0;i<temp.length;i++){
+        if(temp[temp.length-1-i] == v[i])
+            cnt++;
+    }
+    if(cnt == temp.length)
+        answer.push("yes");
+    else
+        answer.push("no");
+});
+console.log(answer.join("\n"));
