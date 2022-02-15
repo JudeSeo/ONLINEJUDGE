d4442fb4a9e99b83dc7ee038d014337e6356c1cf
JudeSeo <gn0408@gmail.com>
2022-02-15 21:02:09 +0900

    백준 단어길이재기

---
+++ BackJoon/BB2_measuringWordLength.js
@@ -0,0 +1,6 @@
+// 2022-02-15
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+
+console.log(input[0].length);
