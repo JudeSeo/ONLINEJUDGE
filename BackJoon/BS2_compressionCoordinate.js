22e6a3bfc5f381ed75094d960129a6f738768a7c
JudeSeo <gn0408@gmail.com>
2022-02-15 20:57:23 +0900

    백준 좌표압축

---
+++ BackJoon/BS2_compressionCoordinate.js
@@ -0,0 +1,19 @@
+// 2022-02-15
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+input = input.slice(1)[0].split(" ");
+let temp = Array.from(new Set(input)).sort((a, b) => (a-b));
+let answer = [];
+let object = {};
+
+temp.forEach((v, i) => (object[v]=i));
+for(let i=0;i<input.length;i++)
+    answer.push(object[input[i]]);
+
+// input.forEach((value, index)=>{
+//     answer[index] = temp.filter((v) => (
+//         v < value)).length})
+// 뭔 차이로 메모리초과가 뜨는지 모르겠다..
+
+console.log(answer.join(" "));
