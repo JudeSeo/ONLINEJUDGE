0000edf2c2001832fb9419f194152ffaa7cc07b0
JudeSeo <gn0408@gmail.com>
2022-02-15 20:58:26 +0900

    백준 괄호

---
+++ BackJoon/BS4_parenthesis.js
@@ -0,0 +1,24 @@
+// 2022-02-15
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+input = input.slice(1);
+let answer = [];
+input.forEach((v, i)=>{
+    let temp = v.split("");
+    let cnt = 0;
+    for(let i=0;i<temp.length;i++){
+        if(temp[i] == "(")
+            cnt++;
+        else if(temp[i] == ")")
+            cnt--;
+        if(cnt < 0)
+            break;
+    }
+    if(cnt == 0)
+        answer.push("YES");
+    else
+        answer.push("NO")
+});
+
+console.log(answer.join("\n"));
