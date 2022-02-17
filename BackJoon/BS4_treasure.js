a4a966c954ab7cd6d95201107ba8ccacde27570f
JudeSeo <gn0408@gmail.com>
2022-02-17 22:43:03 +0900

    백준 보물

---
+++ BackJoon/BS4_treasure.js
@@ -0,0 +1,14 @@
+// 2022-02-17
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let len = parseInt(input[0]);
+let a = input[1].split(" ")
+let b = input[2].split(" ")
+a.sort((a, b) => (a - b));
+b.sort((a, b) => (b - a));
+let answer = 0;
+for (let i = 0; i < len; i++) {
+    answer += (parseInt(a[i]) * parseInt(b[i]));
+}
+console.log(answer);
