49551214897a0a7238bb46c64944c6e9f29c2aa4
JudeSeo <gn0408@gmail.com>
2022-02-22 21:23:30 +0900

    백준 8진수 2진수

---
+++ BackJoon/Bronze/BB3_octalBinary.js
@@ -0,0 +1,17 @@
+// 2022-02-22
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+input = input[0];
+let answer = [];
+for (let i = 0; i < input.length; i++) {
+    let temp = parseInt(input[i], 8).toString(2);
+    while (temp.length < 3) {
+        if (i == 0)
+            break;
+        else
+            temp = "0" + temp;
+    }
+    answer.push(temp);
+}
+console.log(answer.join(""));
