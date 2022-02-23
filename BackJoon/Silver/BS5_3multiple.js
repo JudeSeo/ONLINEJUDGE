ee7ba1916c79424b936b5c69acd788592b702477
JudeSeo <gn0408@gmail.com>
2022-02-23 20:01:56 +0900

    백준 3의배수

---
+++ BackJoon/Silver/BS5_3multiple.js
@@ -0,0 +1,21 @@
+// 2022-02-23
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+input = input[0];
+let answer = [];
+let cnt = 0;
+while (parseInt(input) >= 10) {
+    let sum = 0;
+    for (let i = 0; i < input.length; i++) {
+        sum += parseInt(input[i]);
+    }
+    input = sum.toString();
+    cnt++;
+}
+answer.push(cnt);
+if (input == 3 || input == 6 || input == 9)
+    answer.push("YES");
+else
+    answer.push("NO")
+console.log(answer.join("\n"));
