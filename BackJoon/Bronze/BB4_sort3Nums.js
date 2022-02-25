bbe9e1ab770002d869c17a9b9421f5ef11034336
JudeSeo <gn0408@gmail.com>
2022-02-25 20:42:10 +0900

    백준 세수정렬

---
+++ BackJoon/Bronze/BB4_sort3Nums.js
@@ -0,0 +1,6 @@
+// 2022-02-25
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ");
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ");
+input.sort((a, b)=>(a-b))
+console.log(input.join(" "))
