9517d616ded2b65417b1b8bb13e1b565377922cc
JudeSeo <gn0408@gmail.com>
2022-03-01 21:52:54 +0900

    백준 구간합구하기4

---
+++ BackJoon/Silver/BS3_blockSum4.js
@@ -0,0 +1,20 @@
+// 2022-03-01
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = [];
+let nums = input[1].split(" ");
+input = input.slice(2);
+input.forEach((v, index) => {
+    let temp = v.split(" ");
+    let sums = 0;
+    let i = parseInt(temp[0])-1
+    while (i < parseInt(temp[1])){
+        sums += parseInt(nums[i])
+        i++;
+    }
+    answer.push(sums);
+});
+console.log(answer.join("\n"));
+
+// 시간초과 나온다....
