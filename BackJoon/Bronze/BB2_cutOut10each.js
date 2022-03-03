504e2b9b7fba050f093bb5e4082b507441000f1b
JudeSeo <gn0408@gmail.com>
2022-03-03 21:06:38 +0900

    백준 열 개씩 끊어 출력하기

---
+++ BackJoon/Bronze/BB2_cutOut10each.js
@@ -0,0 +1,10 @@
+// 2022-03-03
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+let answer = [];
+while(input.length > 0){
+    answer.push(input.slice(0,10));
+    input = input.slice(10);
+}
+console.log(answer.join("\n"))
