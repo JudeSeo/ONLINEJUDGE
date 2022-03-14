3c1c8760b4ab62ef538f2cb4b9ea44dd0fcc6792
JudeSeo <gn0408@gmail.com>
2022-03-14 19:42:23 +0900

    백준 별찍기13

---
+++ BackJoon/Bronze/BB3_printStars13.js
@@ -0,0 +1,13 @@
+// 2022-03-14
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+let answer = [];
+for (let i = 1; i <= input; i++) {
+    answer[i] = "*".repeat(i);
+    if (i != input) {
+        answer[(input) * 2 - i] = "*".repeat(i);
+    }
+}
+answer = answer.slice(1);
+console.log(answer.join("\n"));
