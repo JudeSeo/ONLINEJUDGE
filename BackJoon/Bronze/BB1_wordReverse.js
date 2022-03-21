3b513e4b36362dfaaec19acdc0147d01a0466ed4
JudeSeo <gn0408@gmail.com>
2022-03-21 20:20:53 +0900

    백준 단어뒤집기

---
+++ BackJoon/Bronze/BB1_wordReverse.js
@@ -0,0 +1,13 @@
+// 2022-03-21
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').slice(1);
+input.forEach((v, i) => {
+    let temp = v.split(" ");
+    let answer = [];
+    temp.forEach((value, index) => {
+        value = value.split("").reverse().join("");
+        answer.push(value);
+    });
+    console.log(answer.join(" "));
+});
