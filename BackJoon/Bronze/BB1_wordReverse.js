bd27c00f1760a96706bd710d1131fec995cd60f4
JudeSeo <gn0408@gmail.com>
2022-03-21 20:21:20 +0900

    단어뒤집기 수정

--- BackJoon/Bronze/BB1_wordReverse.js
+++ BackJoon/Bronze/BB1_wordReverse.js
@@ -1,6 +1,6 @@
 // 2022-03-21
 var fs = require('fs');
-// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
 var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').slice(1);
 input.forEach((v, i) => {
     let temp = v.split(" ");
