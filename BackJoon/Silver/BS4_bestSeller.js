6b6ad488660ed17e793e2e6fe0e01988dafa4ca3
JudeSeo <gn0408@gmail.com>
2022-03-22 19:26:52 +0900

    백준 베스트셀러

---
+++ BackJoon/Silver/BS4_bestSeller.js
@@ -0,0 +1,13 @@
+// 2022-03-22
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+input = input.slice(1);
+const result = {};
+input.forEach((x) => {
+    result[x] = (result[x] || 0) + 1;
+});
+let arr = Object.entries(result);
+let max = Math.max(...Object.values(result));
+let answer = arr.filter((v, i) => (v[1] == max)).sort()[0];
+console.log(answer[0]);
