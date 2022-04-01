996251d1f5a52218d92a9aa4f288154a20a18e01
JudeSeo <gn0408@gmail.com>
2022-04-01 21:58:25 +0900

    백준 hashing

---
+++ BackJoon/Bronze/BB2_hashing.js
@@ -0,0 +1,16 @@
+// 2022-04-01
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+input = input.slice(1)[0].split("");
+let hash = 0;
+let r = 1;
+input.forEach((v, i) => {
+    hash += (v.charCodeAt(0) - 96) * r;
+    hash %= 1234567891;
+    r *= 31
+    r %= 1234567891;
+})
+console.log(hash);
+
+// 뭐가 다른건지 잘 모르겠다...
