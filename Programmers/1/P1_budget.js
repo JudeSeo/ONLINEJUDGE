8158bbf34d14bd1eb2e273516c7e2f0776b5855a
JudeSeo <gn0408@gmail.com>
2022-04-04 21:27:06 +0900

    프로그래머스 예산

---
+++ Programmers/1/P1_budget.js
@@ -0,0 +1,16 @@
+// 2022-04-04
+function solution(d, budget) {
+    var answer = 0;
+    d.sort((a, b) => (a - b));
+    d.forEach((v, i) => {
+        if (budget > parseInt(v)) {
+            budget -= parseInt(v);
+            answer++;
+        }
+    });
+
+    console.log(answer);
+    return answer;
+}
+
+solution([1, 3, 2, 5, 4], 9);
