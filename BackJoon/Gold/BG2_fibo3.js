602a734821b50778639caa78b8a6c59af2006d85
JudeSeo <gn0408@gmail.com>
2022-04-06 21:17:06 +0900

    백준 피보나치 수3

---
+++ BackJoon/Gold/BG2_fibo3.js
@@ -0,0 +1,9 @@
+// 2022-04-06
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+let answer = [0, 1];
+for (let i = 2; i <= input; i++) {
+    answer[i] = ((BigInt(answer[i - 1]) + BigInt(answer[i - 2])) % 1000000n);
+}
+console.log(answer[input].toString());
