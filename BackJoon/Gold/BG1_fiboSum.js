46074062cc6d548a3259ca9548c61e307aa36e19
JudeSeo <gn0408@gmail.com>
2022-04-06 21:16:31 +0900

    백준 피보나치 수의 합

---
+++ BackJoon/Gold/BG1_fiboSum.js
@@ -0,0 +1,15 @@
+// 2022-04-06
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ");
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ");
+input.sort((a, b) => (a - b))
+let max = Number(input[1])+2;
+let answer = [0, 1];
+for (let i = 2; i <= max; i++) {
+    answer[i] = ((BigInt(answer[i - 1]) + BigInt(answer[i - 2])) % 1000000000n);
+}
+let sum = 0n;
+sum = answer[Number(input[1]) + 2] - answer[Number(input[0]) + 1];
+console.log(sum.toString());
+
+// 메모리초과 떳다....
