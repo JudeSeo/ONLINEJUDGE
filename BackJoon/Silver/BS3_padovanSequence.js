66f58c50961e97438725ba3c92c6cf24a4f1516b
JudeSeo <gn0408@gmail.com>
2022-04-07 22:02:00 +0900

    백준 파도반수열

---
+++ BackJoon/Silver/BS3_padovanSequence.js
@@ -0,0 +1,13 @@
+// 2022-04-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = [1, 1, 1];
+input = input.slice(1);
+let max = Math.max(...input);
+for (let i = 0; i < max; i++) {
+    answer[i + 3] = answer[i] + answer[i + 1];
+}
+input.forEach((v, i) => {
+    console.log(answer[v-1]);
+})
