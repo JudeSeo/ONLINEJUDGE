53c9efb5f084fea7088b4218d9b2c125d0d02d66
JudeSeo <gn0408@gmail.com>
2022-04-07 22:05:20 +0900

    백준 거스름돈

---
+++ BackJoon/Silver/BS5_change.js
@@ -0,0 +1,16 @@
+// 2022-04-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+let answer = 0;
+if (input == '1' ||input == '3')
+    answer = -1;
+else {
+    while (input % 5 != 0) {
+        input -= 2
+        answer++;
+    }
+    answer += (input /= 5);
+}
+
+console.log(answer);
