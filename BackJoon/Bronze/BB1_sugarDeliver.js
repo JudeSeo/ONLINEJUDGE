028feb74b5d112fc7928911c838c26c39f5f5c6e
JudeSeo <gn0408@gmail.com>
2022-04-10 20:54:01 +0900

    백준 설탕배달

---
+++ BackJoon/Bronze/BB1_sugarDeliver.js
@@ -0,0 +1,18 @@
+// 2022-04-10
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+let answer = 0;
+while (input >= 3) {
+    if (input % 5 == 0) {
+        answer += input / 5;
+        input = 0;
+    } else {
+        input -= 3;
+        answer++;
+    }
+}
+if (input != 0)
+    answer = -1;
+
+console.log(answer); 
