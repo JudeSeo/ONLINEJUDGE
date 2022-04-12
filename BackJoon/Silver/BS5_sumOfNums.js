cef67223d40bce75a48bd36dafc348cf94a32ee8
JudeSeo <gn0408@gmail.com>
2022-04-12 19:44:05 +0900

    백준 수들의합

---
+++ BackJoon/Silver/BS5_sumOfNums.js
@@ -0,0 +1,11 @@
+// 2022-04-12
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+let answer = 0;
+let temp = 0
+while (temp <= input) {
+    answer++
+    temp = (answer * (answer + 1)) / 2;
+}
+console.log(answer - 1);
