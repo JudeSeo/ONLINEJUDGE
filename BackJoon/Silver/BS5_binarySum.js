521e4ec8c5f02e756a7a607104b635c591cb40b6
JudeSeo <gn0408@gmail.com>
2022-04-17 16:09:10 +0900

    백준 이진수 덧셈

---
+++ BackJoon/Silver/BS5_binarySum.js
@@ -0,0 +1,17 @@
+// 2022-04-17
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = [];
+input = input.slice(1);
+input.forEach((v, i)=>{
+    let temp = v.split(" ");
+    let a = parseInt(temp[0], 2);
+    let b = parseInt(temp[1], 2)
+    let sum = BigInt(a) + BigInt(b);
+    answer.push(sum.toString(2));
+})
+
+console.log(answer.join("\n"));
+
+// 이렇게 풀어도 될거 같은데 왜 틀린지 모르겠다....
