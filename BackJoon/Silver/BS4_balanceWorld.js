395cd00ba6fb856531ce70ea2f848dfb5f7bfdbf
JudeSeo <gn0408@gmail.com>
2022-04-22 20:12:05 +0900

    백준 균형잡힌세계

---
+++ BackJoon/Silver/BS4_balanceWorld.js
@@ -0,0 +1,36 @@
+// 2022-04-22
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+input.forEach(v => {
+    let stack = [];
+    let answer = "no";
+    let check = true;
+    if (v[0] == '.')
+        return;
+
+    for (let i = 0; i < v.length; i++) {
+        switch (v[i]) {
+            case '(':
+                stack.push(v[i]);
+                break;
+            case ')':
+                if (stack[stack.length - 1] != '(')
+                    check = false;
+                stack.pop();
+                break;
+            case '[':
+                stack.push(v[i]);
+                break;
+            case ']':
+                if (stack[stack.length - 1] != '[')
+                    check = false;
+                stack.pop();
+                break;
+        }
+    }
+    if (stack.length == 0 && check)
+        answer = "yes";
+
+    console.log(answer);
+})
