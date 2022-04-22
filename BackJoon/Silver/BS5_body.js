fc12356d4ef87b2476c893987d5e023ac17ac18a
JudeSeo <gn0408@gmail.com>
2022-04-22 20:12:41 +0900

    백준 덩치

---
+++ BackJoon/Silver/BS5_body.js
@@ -0,0 +1,19 @@
+// 2022-04-22
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = [];
+input = input.slice(1);
+for (let i = 0; i < input.length; i++) {
+    let grade = 1;
+    let [aTall, aWeight] = input[i].split(" ").map(v => Number(v));
+    for (let j = 0; j < input.length; j++) {
+        let [bTall, bWeight] = input[j].split(" ").map(v => Number(v));
+        if (i !== j) {
+            if (aTall < bTall && aWeight < bWeight)
+                grade++;
+        }
+    }
+    answer.push(grade);
+}
+console.log(answer.join(" "));
