c69b457a09cbf62875c4cbc507ba68175dc17e01
JudeSeo <gn0408@gmail.com>
2022-04-24 13:45:24 +0900

    백준 숫자카드2

---
+++ BackJoon/Silver/BS4_numCard2.js
@@ -0,0 +1,15 @@
+// 2022-04-24
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let n = input[1].split(" ");
+let m = input[3].split(" ");
+let result = {};
+n.sort((a, b) => (a - b)).forEach((x) => {
+    result[x] = (result[x] || 0) + 1;
+});
+let answer = new Array(m.length).fill(0);
+m.forEach((v, i) => {
+    answer[i] = result[v] == undefined ? 0 : result[v];
+});
+console.log(answer.join(" "));
