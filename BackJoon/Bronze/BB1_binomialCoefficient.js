882d12955dc86046a3808068ed629ca8b5a13895
JudeSeo <gn0408@gmail.com>
2022-04-25 21:04:05 +0900

    백준 이항계수1

---
+++ BackJoon/Bronze/BB1_binomialCoefficient.js
@@ -0,0 +1,12 @@
+// 2022-04-25
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ");
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ");
+let n = input[0];
+let k = input[1];
+let sum = 1;
+
+for (let i = 1; i <= k; i++) {
+    sum *= ((n - i + 1) / i);
+}
+console.log(sum);
