1de865fe03b2f37282d5c2176dee581b01c812c5
JudeSeo <gn0408@gmail.com>
2022-04-25 21:07:02 +0900

    백준 팩토리얼 0의 개수

---
+++ BackJoon/Silver/BS4_NumberOfFactory0s.js
@@ -0,0 +1,19 @@
+// 2022-04-25
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+input = BigInt(input)
+let answer = 1n;
+while (input > 0) {
+    answer *= input;
+    input--;
+}
+answer = answer.toString().split("").map(v => (Number(v))).reverse();
+let ans = 0;
+for (let i = 0; i < answer.length; i++) {
+    if(answer[i] !== 0){
+        ans = i;
+        break;
+    }
+}
+console.log(ans);
