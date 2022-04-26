7916853bd02868513f96649b02c3fb0139c9f904
JudeSeo <gn0408@gmail.com>
2022-04-26 19:31:08 +0900

    백준 소수구하기

---
+++ BackJoon/Silver/BS3_findPrime.js
@@ -0,0 +1,20 @@
+// 2022-04-26
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v => Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+var answer = [];
+var primeChk = [0, 0, 2, 3];
+for (let i = 2; i <= input[1]; i++) {
+    for (let j = 2; j <= Math.sqrt(i); j++) {
+        if (i != j && i % j == 0) {
+            primeChk[i] = 0;
+            break;
+        } else
+            primeChk[i] = i;
+    }
+}
+for (let i = input[0]; i <= input[1]; i++) {
+    if (primeChk[i] > 0)
+        answer.push(primeChk[i]);
+}
+console.log(answer.join("\n"));
