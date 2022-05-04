b0f038ffd609956a9633b122665c2e066c07529e
JudeSeo <gn0408@gmail.com>
2022-05-04 21:07:05 +0900

    백준 숫자카드

--- BackJoon/Silver/BS4_numCard.js
+++ BackJoon/Silver/BS4_numCard.js
@@ -1,17 +1,17 @@
-// 2022-02-21
+// 2022-02-21 -> 2022-05-04
 var fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
 let n = input[1].split(" ");
 let m = input[3].split(" ");
-n.sort((a, b)=> (a-b));
+let result = {};
+n.sort((a, b) => (a - b)).forEach((x) => {
+    result[x] = (result[x] || 0) + 1;
+});
 let answer = new Array(m.length).fill(0);
-n.forEach((v, i)=>{
-    let index = m.indexOf(v)
-    if (index >= 0) {
-        answer[index] = 1;
-    }
+m.forEach((v, i) => {
+    answer[i] = result[v] == undefined ? 0 : result[v];
 });
 console.log(answer.join(" "));
 
-// 이분탐색 사용 필요
+// 이분탐색 사용 필요하지만 forEach도 가능!
