fca3d83cf5058a42292484582ba94a40bee43b21
JudeSeo <gn0408@gmail.com>
2022-05-29 21:13:05 +0900

    백준 쉽게 푸는 문제

---
+++ BackJoon/Silver/BS5_easySolveProb.js
@@ -0,0 +1,18 @@
+// 2022-05-29
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+let start = input.split(" ")[0];
+let end = input.split(" ")[1];
+let answer = 0;
+let arr = []
+for (let i = 1; i <= end; i++) {
+    for (let j = 1; j <= i; j++) {
+        arr.push(i);
+    }
+}
+for (let i = start - 1; i < end; i++) {
+    answer += arr[i]
+}
+
+console.log(answer)
