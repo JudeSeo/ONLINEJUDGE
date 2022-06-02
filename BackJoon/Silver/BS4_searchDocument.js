0ccb3afe6260dbc2c93be0b3cbe6e8d3e7d15976
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-02 16:28:02 +0900

    백준 문서검색

---
+++ BackJoon/Silver/BS4_searchDocument.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let string = input[0];
+let word = input[1];
+let answer = 0;
+while (string.indexOf(word) !== -1) {
+    string = string.replace(word, "0");
+    answer++;
+}
+console.log(answer);
