81e387d21e629ce3d9b5ffdaab5819d3d98456aa
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-08 14:08:30 +0900

    백준 UCPC는 무엇의 약자일까?

---
+++ BackJoon/Silver/BS5_whatShortIsUCPC.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+let ucpc = ["U", "C", "P", "C"];
+let answer = [];
+let arr = [];
+let i = 0;
+let flag = true;
+while (flag) {
+    if (input.indexOf(ucpc[i]) >= 0) {
+        arr.push(input.indexOf(ucpc[i]));
+        input = input.slice((input.indexOf(ucpc[i])) + 1)
+        i++;
+    } else{
+        flag = false;
+        break;
+    }
+}
+
+if (arr.length == 4)
+    answer = "I love UCPC";
+else
+    answer = "I hate UCPC"
+console.log(answer);
