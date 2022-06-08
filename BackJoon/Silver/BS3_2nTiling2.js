55f0ffadc8670b0d223a12cecb641aa5c81c3733
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-09 08:18:01 +0900

    백준 2×n 타일링 2

---
+++ BackJoon/Silver/BS3_2nTiling2.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+input = Number(input);
+let answer = [1n, 1n];
+for (let i = 0; i <= (input + 1); i++) {
+    answer.push(((2n * answer[i]) + answer[i + 1]) % 10007n);
+}
+console.log(answer[input].toString());
