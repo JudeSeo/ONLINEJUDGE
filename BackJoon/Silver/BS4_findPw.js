4214c2531193621f649de82775c1db8f6d6517da
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-08 16:23:11 +0900

    백준 비밀번호 찾기

---
+++ BackJoon/Silver/BS4_findPw.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = [];
+let siteNum = input[0].split(" ")[0];
+let sites = []
+input = input.slice(1);
+let finds = input.slice(siteNum);
+
+input.slice(0, siteNum).map(v=>{
+    let [ip, pw] = v.split(" ");
+    sites[ip]=pw;
+})
+finds.map(v => {
+    answer.push(sites[v])
+})
+console.log(answer.join("\n"));
