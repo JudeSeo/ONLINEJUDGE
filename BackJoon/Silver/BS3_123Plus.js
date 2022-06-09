50f532df872d0629213bb921b3aec3d7db994fc4
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-09 11:03:21 +0900

    백준 1,2,3 더하기

---
+++ BackJoon/Silver/BS3_123Plus.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = [];
+let arr = [1, 1, 2];
+input = input.slice(1).map(v => Number(v));
+let max = Math.max(...input);
+for (let i = 3; i <= max; i++) {
+    arr[i] = arr[i - 3] + arr[i - 2] + arr[i - 1]
+};
+input.forEach(v => {
+    answer.push(arr[v])
+})
+console.log(answer.join("\n"));
