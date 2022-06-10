6eae5fc7d90aa51d63d3d116010267e2ebca7470
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-10 14:38:12 +0900

    백준 카드

---
+++ BackJoon/Silver/BS4_card.java
@@ -0,0 +1,40 @@
+// 2022-06-10
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num1 = Integer.parseInt(st.nextToken());
+        long[] numArr = new long[num1];
+
+        for (int i = 0; i < num1; i++) {
+            st = new StringTokenizer(br.readLine());
+            numArr[i] = Long.parseLong(st.nextToken());
+        }
+        Arrays.sort(numArr);
+        long max = numArr[0];
+        long num = 1, cnt = 1;
+        for (int i = 1; i < num1; i++) {
+            if (numArr[i] == numArr[i - 1])
+                cnt++;
+            else
+                cnt = 1;
+            if (cnt > num) {
+                num = cnt;
+                max = numArr[i];
+            }
+        }
+
+        sb.append(max);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
