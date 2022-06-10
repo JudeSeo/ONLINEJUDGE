0884b743c6661fb9acdd31bf0182f47c75e163b9
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-10 09:02:59 +0900

    백준 합 구하기

---
+++ BackJoon/Silver/BS3_getSum.java
@@ -0,0 +1,33 @@
+// 2022-06-10
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num1 = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        long[] numSet = new long[num1 + 1];
+        for (int i = 1; i <= num1; i++) {
+            numSet[i] = numSet[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < num2; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            sb.append(numSet[to] - numSet[from-1] + "\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
