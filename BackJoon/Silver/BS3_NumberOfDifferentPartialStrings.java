262186f25ca47babe63b8db5911f5abd449cb599
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-13 09:29:28 +0900

    백준 서로 다른 부분 문자열의 개수

---
+++ BackJoon/Silver/BS3_NumberOfDifferentPartialStrings.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Set set = new TreeSet();
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = st.nextToken();
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                set.add(str.substring(i, j));
+            }
+        }
+        sb.append(set.size());
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
