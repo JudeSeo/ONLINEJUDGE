967634c56efb916c02a4f3827c1811e40b289eca
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-13 10:19:14 +0900

    백준 파일정리

---
+++ BackJoon/Silver/BS3_cleanUpFile.java
@@ -0,0 +1,29 @@
+// 2022-06-13
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Map<String, Integer> map = new TreeMap();
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < num; i++) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken().split("[.]")[1];
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+
+        for (String s : map.keySet()) {
+            sb.append(s).append(" ").append(map.get(s)).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
