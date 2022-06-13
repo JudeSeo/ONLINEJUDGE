1d863ba73f0b29b832c17211c3818b5b56bf50e8
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-13 09:12:42 +0900

    백준 생태학

---
+++ BackJoon/Silver/BS2_ecology.java
@@ -0,0 +1,28 @@
+// 2022-06-13
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Map<String, Double> map = new TreeMap();
+        StringBuilder sb = new StringBuilder();
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = "";
+        double cnt = 0;
+        while ((str = br.readLine()) != null) {
+            map.put(str, map.getOrDefault(str, 0.0) + 1);
+            cnt++;
+        }
+        for (String s : map.keySet()) {
+            String rate = String.format("%.4f", (map.get(s) * 1000000 / cnt / 10000.0));
+            sb.append(s).append(" ").append(rate).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
