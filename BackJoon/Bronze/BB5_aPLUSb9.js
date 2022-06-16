64f6e93d529cba5112fd9d41617f0b4f3cb3eb29
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-16 15:10:27 +0900

    백준 a + b - 9

---
+++ BackJoon/Bronze/BB5_aPLUSb9.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+input = input.split(" ").map(v => BigInt(v));
+
+console.log((input[0] + input[1]).toString());
