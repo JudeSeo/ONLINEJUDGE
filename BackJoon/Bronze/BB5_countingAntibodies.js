2355d9aaebde80a588296e5b0aee642bf1a364f1
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-20 13:22:53 +0900

    백준 Counting Antibodies

---
+++ BackJoon/Bronze/BB5_countingAntibodies.js
@@ -0,0 +1,9 @@
+// 2022-06-20
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let a = input[0].split(" ");
+let b = input[1].split(" ");
+let c = input[2].split(" ");
+let answer = (a[0] * a[1] + b[0] * b[1]) * (c[0] * c[1] * c[2]);
+console.log(answer);
