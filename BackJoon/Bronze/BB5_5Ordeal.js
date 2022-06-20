fa6011da9ce38f4a2ba6ad98eb60b940b33bb838
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-20 09:09:51 +0900

    백준 5의 수난

---
+++ BackJoon/Bronze/BB5_5Ordeal.js
@@ -0,0 +1,9 @@
+// 2022-06-20
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split("").map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split("").map(v => Number(v));
+let answer = 0
+input.forEach((v, i) => {
+    answer += v ** 5;
+})
+console.log(answer);
