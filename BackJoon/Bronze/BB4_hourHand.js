6ae14f9f069eeb731e7c6ac793e53455295552db
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-21 10:19:32 +0900

    백준 短針 (Hour Hand)

---
+++ BackJoon/Bronze/BB4_hourHand.js
@@ -0,0 +1,9 @@
+// 2022-06-21
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').map(v => Number(v));
+let answer = input[0] + input[1]
+while(answer > 12){
+    answer -= 12
+}
+console.log(answer);
