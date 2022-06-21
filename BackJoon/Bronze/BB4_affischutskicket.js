ac4bc8c3e648867125287d78ed432c480092ca93
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-21 15:28:40 +0900

    백준 Affischutskicket

---
+++ BackJoon/Bronze/BB4_affischutskicket.js
@@ -0,0 +1,9 @@
+// 2022-06-21
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ");
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ");
+let c4 = 0.229 * 0.324;
+let a3 = 0.297 * 0.42
+let a4 = 0.210 * 0.297;
+let answer = input[0] * 2 * c4 + input[1] * 2 * a3 + input[2] * a4
+console.log(answer.toFixed(6));
