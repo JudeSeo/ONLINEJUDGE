9d92cfd9fb780e0b133cf79f8a474aeb4614cd30
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-21 10:30:00 +0900

    백준 Betting

---
+++ BackJoon/Bronze/BB4_betting.js
@@ -0,0 +1,6 @@
+// 2022-06-21
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+let answer = [(100 / input).toFixed(10), (100 / (100 - input)).toFixed(10)];
+console.log(answer.join("\n"));
