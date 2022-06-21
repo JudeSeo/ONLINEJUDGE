69f76dc55c2efb167f415b0c543fa66e9a1be9a7
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-22 08:27:54 +0900

    백준 치킨댄스를 추는 곰곰이를 본 임스

---
+++ BackJoon/Bronze/BB4_chicken.js
@@ -0,0 +1,9 @@
+// 2022-06-22
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let chicken = Number(input.slice(0, 1));
+input = input.slice(1)[0].split(" ").map(v => Number(v));
+let answer = 0;
+answer = parseInt(input[0] / 2) + input[1];
+console.log(answer > chicken ? chicken : answer);
