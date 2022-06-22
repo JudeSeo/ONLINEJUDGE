3599d28429f528f8c64311579e3633bc74da966f
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-23 08:50:16 +0900

    백준 시험 점수

---
+++ BackJoon/Bronze/BB4_examScore.js
@@ -0,0 +1,10 @@
+// 2022-06-23
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = [];
+input.forEach((v, i) => {
+    let temp = v.split(" ").map(v => Number(v));
+    answer.push(temp[0] + temp[1] + temp[2] + temp[3])
+})
+console.log(Math.max(...answer));
