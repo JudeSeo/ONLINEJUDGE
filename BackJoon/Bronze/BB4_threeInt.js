7d8b70e9c320ddf0729723cfa4a8604258b33e18
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-24 16:26:20 +0900

    백준 3 つの整数 (Three Integers)

---
+++ BackJoon/Bronze/BB4_threeInt.js
@@ -0,0 +1,6 @@
+// 2022-06-24
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ").map(v=>Number(v));
+input.sort();
+console.log(input[1]);
