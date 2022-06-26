5cdba812cd5096dd3e294b741e165d03267a19e5
JudeSeo <gn0408@gmail.com>
2022-06-26 20:01:19 +0900

    정렬 수정

---
+++ BackJoon/Bronze/BB4_barn.js
@@ -0,0 +1,10 @@
+// 2022-06-26
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let cow = input[0];
+let height = input[1];
+let width = input[2];
+let square = input[3];
+let answer = parseInt(height / square) * parseInt(width / square)
+console.log(answer > cow ? cow : answer);
