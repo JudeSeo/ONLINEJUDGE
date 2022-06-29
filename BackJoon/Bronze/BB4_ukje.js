90c7c15a8da4b6614e0bd3a44a085f04726c328f
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-06-29 09:35:16 +0900

    백준 욱 제

---
+++ BackJoon/Bronze/BB4_ukje.js
@@ -0,0 +1,7 @@
+// 2022-06-29
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ");
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ");
+let m = (input[1]-input[0])/400;
+let answer = 1/(1+(10**m));
+console.log(answer);
