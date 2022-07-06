c02ee3a257105c6517cab70149f06144b2721ea1
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-06 11:41:19 +0900

    백준 전자레인지

---
+++ BackJoon/Bronze/BB4_microwave.js
@@ -0,0 +1,15 @@
+// 2022-07-06
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+let answer = [];
+if (input % 10 != 0)
+    answer.push(-1);
+else {
+    answer.push(parseInt(input / 300));
+    input %= 300;
+    answer.push(parseInt(input / 60));
+    input %= 60;
+    answer.push(parseInt(input / 10));
+}
+console.log(answer.join(" "));
