56ce055b2e417fff1f3c794fa34a4fab6be706a1
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-07 08:41:05 +0900

    백준 과목선택

---
+++ BackJoon/Bronze/BB4_subjectChoice.js
@@ -0,0 +1,10 @@
+// 2022-07-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let science = input.slice(0, 4).map(v=>Number(v));
+let society = input.slice(4).map(v=>Number(v));
+let answer = science.reduce((a, b)=>a+b);
+answer -= Math.min(...science);
+answer += Math.max(...society);
+console.log(answer);
