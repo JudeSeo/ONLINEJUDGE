0aa3b2670ed01f108a1e82735acc8f3462861bb4
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-06 11:44:58 +0900

    백준 영수증

---
+++ BackJoon/Bronze/BB5_receipt.js
@@ -0,0 +1,14 @@
+// 2022-07-06
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = "Yes";
+let total = input[0];
+input = input.slice(2);
+input.forEach(v=>{
+    let temp = v.split(" ");
+    total -= (temp[0]*temp[1])
+})
+if(total != 0)
+    answer="No";
+console.log(answer);
