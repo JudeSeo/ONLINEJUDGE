6db089032043647c8abf8fd300baa704a39c4a39
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-06 14:45:01 +0900

    수정

--- BackJoon/Bronze/BB4_laptopSticker.js
+++ BackJoon/Bronze/BB4_laptopSticker.js
@@ -1,8 +1,8 @@
 // 2022-07-06
 var fs = require('fs');
-// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ");
-var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ");
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ").map(v=>Number(v));
 let answer = 0;
-if (input[0] - 1 >= input[2] && input[1] - 1 >= input[3])
+if (input[0]-1 > input[2] && input[1]-1 > input[3])
     answer = 1;
 console.log(answer);
