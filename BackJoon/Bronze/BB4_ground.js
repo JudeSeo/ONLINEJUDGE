9d0bac9734bd0698a9df6c81eb7857e0f6e545da
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-07 10:48:20 +0900

    백준 운동장 한 바퀴

---
+++ BackJoon/Bronze/BB4_ground.js
@@ -0,0 +1,6 @@
+// 2022-07-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = (input[0]*2)+(input[1]*2*3.141592)
+console.log(answer);
