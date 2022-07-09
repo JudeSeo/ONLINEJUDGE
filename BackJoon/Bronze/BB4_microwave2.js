128b2529e4b923a87fbdda69f910eeb77ef16b11
JudeSeo <gn0408@gmail.com>
2022-07-10 00:00:54 +0900

    백준 전자레인지

---
+++ BackJoon/Bronze/BB4_microwave2.js
@@ -0,0 +1,11 @@
+// 2022-07-10
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').map(v => Number(v));
+let answer = 0;
+if (input[0] < 0) {
+    answer = (-1 * input[0]) * input[2] + input[3] + input[1] * input[4]
+} else {
+    answer = (input[1] - input[0]) * input[4]
+}
+console.log(answer);
