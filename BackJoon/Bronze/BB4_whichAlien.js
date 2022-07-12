552fdd22369f287cbab691be1e561b0aa286da72
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-12 14:50:23 +0900

    백준 Which Alien

---
+++ BackJoon/Bronze/BB4_whichAlien.js
@@ -0,0 +1,12 @@
+// 2022-07-12
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').map(v => Number(v));
+let answer = [];
+if (input[0] >= 3 && input[1] <= 4)
+  answer.push("TroyMartian");
+if (input[0] <= 6 && input[1] >= 2)
+  answer.push("VladSaturnian");
+if (input[0] <= 2 && input[1] <= 3)
+  answer.push("GraemeMercurian");
+console.log(answer.join("\n"));
