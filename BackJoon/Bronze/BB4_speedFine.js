255e428972b1ce7d9d1e04568195c4293e185ca2
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-13 13:26:00 +0900

    백준 Speed fines are not fine!

---
+++ BackJoon/Bronze/BB4_speedFine.js
@@ -0,0 +1,16 @@
+// 2022-07-13
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').map(v => Number(v));
+let answer = "Congratulations, you are within the speed limit!"
+if (input[0] < input[1]) {
+  let speed = input[1] - input[0]
+  if (speed >= 1 && speed <= 20)
+    answer = "You are speeding and your fine is $100.";
+  else if (speed >= 21 && speed <= 30)
+    answer = "You are speeding and your fine is $270."
+  else
+    answer = "You are speeding and your fine is $500."
+}
+
+console.log(answer);
