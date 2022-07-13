3af95712638387f6e57cfac3a328af7a71c14375
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-13 16:16:22 +0900

    백준 Can you add this?

---
+++ BackJoon/Bronze/BB4_canYouAddThis.js
@@ -0,0 +1,11 @@
+// 2022-07-13
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = [];
+input = input.slice(1);
+input.forEach(v => {
+  let temp = v.split(" ").map(value => Number(value));;
+  answer.push(temp[0] + temp[1])
+})
+console.log(answer.join("\n"));
