cc622a6109c9e9fa4eb631409ad2557eaa594d39
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-13 16:00:45 +0900

    백준 Telemarketer or not?

---
+++ BackJoon/Bronze/BB4_telemarketer.js
@@ -0,0 +1,12 @@
+// 2022-07-13
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').map(v => Number(v));
+let answer = "answer"
+if (input[0] == 8 || input[0] == 9) {
+  if (input[1] == input[2]) {
+    if (input[3] == 8 || input[3] == 9)
+      answer = "ignore";
+  }
+}
+console.log(answer);
