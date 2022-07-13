e0c0e5ebc8db26cdd8cb824138f09c5bd7e4eb6b
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-13 13:18:19 +0900

    백준 Hard choice

---
+++ BackJoon/Bronze/BB4_hardChoice.js
@@ -0,0 +1,11 @@
+// 2022-07-13
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = 0;
+let food = input[0].split(" ");
+let people = input[1].split(" ");
+answer += (people[0] - food[0] <= 0 ? 0 : people[0] - food[0]);
+answer += (people[1] - food[1] <= 0 ? 0 : people[1] - food[1]);
+answer += (people[2] - food[2] <= 0 ? 0 : people[2] - food[2]);
+console.log(answer);
