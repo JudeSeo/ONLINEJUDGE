f22ac14de64c08e1bd1419b246fdd3e5749eca4f
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-14 09:31:48 +0900

    백준 X に最も近い値 (The Nearest Value)

---
+++ BackJoon/Bronze/BB4_theNearestValue.js
@@ -0,0 +1,10 @@
+// 2022-07-14
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = input[0];
+if (answer >= input[2])
+    answer = input[2];
+else if (answer <= input[1])
+    answer = input[1];
+console.log(answer);
