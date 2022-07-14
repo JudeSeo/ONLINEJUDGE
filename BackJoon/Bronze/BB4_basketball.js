8f797b98b3f8e89fea6a2e9914ef9f2a8d7dfac4
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-14 14:05:17 +0900

    백준 Basketball One-on-One

---
+++ BackJoon/Bronze/BB4_basketball.js
@@ -0,0 +1,18 @@
+// 2022-07-14
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split("");
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split("");
+let a = 0;
+let b = 0;
+for (let i = 0; i < (input.length / 2); i++) {
+    switch (input[i * 2]) {
+        case "A":
+            a += Number(input[i * 2 + 1]);
+            break;
+        case "B":
+            b += Number(input[i * 2 + 1]);
+            break;
+    }
+}
+let answer = a > b ? "A" : "B";
+console.log(answer);
