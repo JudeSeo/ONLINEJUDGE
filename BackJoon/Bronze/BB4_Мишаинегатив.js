9cfe51922879a4278f33db3c4695937ab6cd2a52
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-14 13:53:04 +0900

    백준 Миша и негатив

---
+++ BackJoon/Bronze/BB4_Мишаинегатив.js
@@ -0,0 +1,17 @@
+// 2022-07-14
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let line = Number(input[0].split(" ")[0]);
+let basic = input.slice(1, line + 1);
+let compare = input.slice(line + 2);
+let answer = 0;
+basic.forEach((v, i) => {
+    let temp = v.split("")
+    let comp = compare[i].split("")
+    temp.forEach((value, index) => {
+        if(value==comp[index])
+            answer++;
+    })
+})
+console.log(answer);
