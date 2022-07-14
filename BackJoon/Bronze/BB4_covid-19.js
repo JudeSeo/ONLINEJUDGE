1671566bc670a38121c9947181e894bda134631b
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-14 09:54:33 +0900

    백준 Covid-19

---
+++ BackJoon/Bronze/BB4_covid-19.js
@@ -0,0 +1,10 @@
+// 2022-07-14
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = "Yellow";
+if(input[0]<=50 && input[1] <= 10)
+    answer = "White";
+else if(input[1]>30)
+    answer = "Red"
+console.log(answer);
