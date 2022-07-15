36ab0de3bf00b5cd01e3709cda621e6b1d78b2d5
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-15 10:18:27 +0900

    백준 Gnome Sequencing

---
+++ BackJoon/Bronze/BB4_gnomeSequencing.js
@@ -0,0 +1,20 @@
+// 2022-07-15
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+input = input.slice(1);
+let answer = ["Gnomes:"];
+input.forEach((v, i) => {
+    let temp = v.split(" ");
+    let comp = temp.slice(0).sort((a, b)=>a-b);
+    if(JSON.stringify(temp) == JSON.stringify(comp))
+        answer.push("Ordered");
+    else {
+        comp.sort((a, b)=>b-a);
+        if(JSON.stringify(temp) == JSON.stringify(comp))
+            answer.push("Ordered");
+        else
+            answer.push("Unordered")
+    }
+})
+console.log(answer.join("\n"));
