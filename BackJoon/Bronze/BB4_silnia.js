1d03e9e2eaad18c9ee57477698ed2c2deac48522
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-15 11:18:36 +0900

    백준 Silnia

---
+++ BackJoon/Bronze/BB4_silnia.js
@@ -0,0 +1,11 @@
+// 2022-07-15
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0];
+let answer = 1;
+while(input > 0){
+    answer *= input;
+    answer = answer.toString().slice(answer.toString().length-1)
+    input--;
+}
+console.log(answer);
