9480aa86720ae55f5e4f15b3eadeaa8cc3070449
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-15 09:18:46 +0900

    백준 Judging Moose

---
+++ BackJoon/Bronze/BB4_judgingMoose.js
@@ -0,0 +1,16 @@
+// 2022-07-15
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+input.sort((a, b) => b - a);
+let answer = [];
+if (input[0] == 0 && input[1] == 0)
+    answer.push("Not a moose");
+else if (input[0] == input[1]) {
+    answer.push("Even");
+    answer.push(input[0] * 2);
+} else {
+    answer.push("Odd")
+    answer.push(input[0] * 2);
+}
+console.log(answer.join(" "));
