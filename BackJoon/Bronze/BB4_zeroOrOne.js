c0398c3298fbfdd0c790371026c6379925a56b60
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-15 10:08:44 +0900

    백준 Zero or One

---
+++ BackJoon/Bronze/BB4_zeroOrOne.js
@@ -0,0 +1,14 @@
+// 2022-07-15
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ");
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ");
+let answer = "*";
+if (input[0] == input[1] && input[1] == input[2])
+    answer = "*";
+else if (input[0] == input[1])
+    answer = "C";
+else if (input[2] == input[1])
+    answer = "A";
+else if (input[0] == input[2])
+    answer = "B";
+console.log(answer);
