9cefd537aaa10cf44292054be76b379dada8156f
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-18 11:04:57 +0900

    백준 Rulltrappa

---
+++ BackJoon/Bronze/BB4_Rulltrappa.js
@@ -0,0 +1,11 @@
+// 2022-07-18
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let stair = input[0].split(" ");
+let escal = input[1].split(" ");
+let people = input[2].split(" ");
+let s = (stair[0] / stair[1]) + (people[1] / escal[1])
+let w = (stair[0] / stair[2]) + (people[0] / escal[0])
+let answer = s < w ? "latmask" : "friskus"
+console.log(answer);
