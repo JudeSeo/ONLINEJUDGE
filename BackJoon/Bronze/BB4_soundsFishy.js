64132f38d672ee410771c1ef6ec8ec4ac17e546d
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-07-19 09:21:37 +0900

    백준 Sounds fishy!

---
+++ BackJoon/Bronze/BB4_soundsFishy.js
@@ -0,0 +1,14 @@
+// 2022-07-19
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').map(v=>Number(v));
+let answer = "";
+if (input[0] < input[1] && input[1] < input[2] && input[2] < input[3])
+    answer = "Fish Rising";
+else if (input[0] > input[1] && input[1] > input[2] && input[2] > input[3])
+    answer = "Fish Diving";
+else if (input[0] == input[1] && input[1] == input[2] && input[2] == input[3])
+    answer = "Fish At Constant Depth";
+else
+    answer = "No Fish";
+console.log(answer)
