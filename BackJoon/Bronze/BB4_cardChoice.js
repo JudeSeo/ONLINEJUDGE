fa05379d47b42c5d6be514dff339335c704c001c
JudeSeo <gn0408@gmail.com>
2022-07-20 23:02:10 +0900

    백준 카드 뽑기

---
+++ BackJoon/Bronze/BB4_cardChoice.js
@@ -0,0 +1,11 @@
+// 2022-07-20
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let n = input[0];
+let m = input[1];
+let k = input[2];
+let first = m > k ? k : m;
+let second = m > k ? n - m : n - k;
+let answer = first + second;
+console.log(answer);
