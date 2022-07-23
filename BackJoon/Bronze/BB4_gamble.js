902a7ee3aea8d45d631d8257fe01d59bbeca91f6
JudeSeo <gn0408@gmail.com>
2022-07-23 22:37:26 +0900

    백준 사장님 도박은 재미로 하셔야 합니다

---
+++ BackJoon/Bronze/BB4_gamble.js
@@ -0,0 +1,11 @@
+// 2022-07-23
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').map(v => Number(v));
+let answer = 0;
+input.forEach(v => {
+    if (v == -1)
+        return;
+    answer += v;
+})
+console.log(answer);
