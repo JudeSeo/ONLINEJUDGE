eb4380f8454c33cddc01a91ed0b72c0f5fbe9be0
JudeSeo <gn0408@gmail.com>
2022-07-26 18:27:21 +0900

    백준 Schronisko

---
+++ BackJoon/Bronze/BB4_schronisko.js
@@ -0,0 +1,11 @@
+// 2022-07-26
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = [];
+input = input.slice(1);
+input.forEach(v => {
+    let temp = v.split(" ").map(v => Number(v))
+    answer.push(Math.floor((temp[0] * temp[1]) / 2));
+})
+console.log(answer.join("\n"));
