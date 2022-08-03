07ec859997169691dd506b1ae1bb3872b5f8ddb1
JudeSeo <gn0408@gmail.com>
2022-08-03 22:49:58 +0900

    백준 St. Ives

---
+++ BackJoon/Bronze/BB4_St.Ives.js
@@ -0,0 +1,13 @@
+// 2022-08-03
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').map(v => Number(v));
+let answer = [];
+input.forEach(v => {
+    if (v == 0)
+        return;
+    let temp = 0;
+    temp += v ** 4 + v ** 3 + v ** 2 + v ** 1 + 1;
+    answer.push(temp.toFixed(2));
+})
+console.log(answer.join("\n"));
