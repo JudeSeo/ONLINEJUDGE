4fa52686fcd15a7f9dbec1be0250da6006c7f632
JudeSeo <gn0408@gmail.com>
2022-08-05 20:29:48 +0900

    백준 Communication Channels

---
+++ BackJoon/Bronze/BB4_communicationChannel.js
@@ -0,0 +1,14 @@
+// 2022-08-05
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = [];
+input = input.slice(1);
+input.forEach(v => {
+    let temp = v.split(" ")
+    if (temp[0] == temp[1])
+        answer.push("OK");
+    else
+        answer.push("ERROR")
+})
+console.log(answer.join("\n"));
