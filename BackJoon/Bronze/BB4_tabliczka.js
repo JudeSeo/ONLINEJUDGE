eff0bb068bcddc01b3102b29a27c8c122fb6cbb6
JudeSeo <gn0408@gmail.com>
2022-08-10 20:46:49 +0900

    백준 Tabliczka

---
+++ BackJoon/Bronze/BB4_tabliczka.js
@@ -0,0 +1,9 @@
+// 2022-08-10
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(BigInt);
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ").map(BigInt);
+let answer = 0;
+if (input[0] % 2n != 0 && input[1] % 2n != 0)
+    answer = input[0] > input[1] ? input[1] : input[0];
+
+console.log(answer.toString());
