a0247264be6dac51558926e62f6cf163015aff9f
JudeSeo <gn0408@gmail.com>
2022-08-14 23:11:44 +0900

    백준 ZOAC 4

---
+++ BackJoon/Bronze/BB4_ZOAC4.js
@@ -0,0 +1,6 @@
+// 2022-08-14
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = Math.ceil(input[0] / (input[2] + 1)) * Math.ceil(input[1] / (input[3] + 1))
+console.log(answer);
