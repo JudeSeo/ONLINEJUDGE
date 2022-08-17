a28f4f68f2e92659567b1a1f26759b7d27ba6634
JudeSeo <gn0408@gmail.com>
2022-08-17 22:03:52 +0900

    백준 Lunacy

---
+++ BackJoon/Bronze/BB4_lunacy.js
@@ -0,0 +1,11 @@
+// 2022-08-17
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')
+let answer = [];
+input.forEach(v => {
+    if (v == -1.0)
+        return;
+    answer.push(`Objects weighing ${Number(v).toFixed(2)} on Earth will weigh ${(v * 0.167).toFixed(2)} on the moon.`)
+})
+console.log(answer.join("\n"));
