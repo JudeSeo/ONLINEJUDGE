d0fe4956a1533a0a207514b9a439537f7140c15a
JudeSeo <gn0408@gmail.com>
2022-08-20 23:14:32 +0900

    백준 Quality-Adjusted Life-Year

---
+++ BackJoon/Bronze/BB3_quality-AdjustedLife-Year.js
@@ -0,0 +1,11 @@
+// 2022-08-20
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')
+let answer = 0;
+input = input.slice(1)
+input.forEach(v => {
+    let temp = v.split(" ").map(v => Number(v))
+    answer += (temp[0] * temp[1])
+});
+console.log(answer.toFixed(3));
