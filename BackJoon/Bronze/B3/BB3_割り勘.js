868b3bb16c976258aaa8f1f780f6f4fec9fc23a8
JudeSeo <gn0408@gmail.com>
2022-08-21 21:55:23 +0900

    백준 割り勘

---
+++ BackJoon/Bronze/B3/BB3_割り勘.js
@@ -0,0 +1,20 @@
+// 2022-08-21
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n')
+let answer = [];
+while (input.length > 1) {
+    let money = 0;
+    let [n, m] = input[0].split(" ")
+    let num = Number(m / n);
+    let temp = input[1].split(" ").map(v => Number(v));
+    temp.forEach(v => {
+        if (v >= num)
+            money += num;
+        else
+            money += v
+    })
+    input = input.slice(2)
+    answer.push(money)
+}
+console.log(answer.join("\n"));
