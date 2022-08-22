337756280eecba9d505807392c06713fc818fb3e
JudeSeo <gn0408@gmail.com>
2022-08-22 20:03:07 +0900

    백준 XMAS

---
+++ BackJoon/Bronze/B3/BB3_xmas.js
@@ -0,0 +1,11 @@
+// 2022-08-22
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').map(v => Number(v));
+let answer = [];
+const num = input[0];
+input = input.slice(1);
+for (let i = 1; i <= num; i++) {
+    answer.push(input.indexOf(i) + 1)
+}
+console.log(answer.join("\n"));
