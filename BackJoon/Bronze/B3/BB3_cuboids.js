5d011415bbc1dd2e5dfb399f6aca3272c77f628f
JudeSeo <gn0408@gmail.com>
2022-08-23 18:44:58 +0900

    백준 Cuboids

---
+++ BackJoon/Bronze/B3/BB3_cuboids.js
@@ -0,0 +1,22 @@
+// 2022-08-23
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = [];
+input.forEach(v => {
+    if (v == "0 0 0 0")
+        return;
+    let temp = v.split(" ").map(val => Number(val));
+    let arr = [];
+    if (temp[3] == 0)
+        temp[3] = temp[0] * temp[1] * temp[2];
+    else if (temp[0] == 0)
+        temp[0] = temp[3] / (temp[1] * temp[2])
+    else if (temp[1] == 0)
+        temp[1] = temp[3] / (temp[0] * temp[2])
+    else if (temp[2] == 0)
+        temp[2] = temp[3] / (temp[1] * temp[0])
+    arr.push(temp[0], temp[1], temp[2], temp[3]);
+    answer.push(arr.join(" "))
+})
+console.log(answer.join("\n"));
