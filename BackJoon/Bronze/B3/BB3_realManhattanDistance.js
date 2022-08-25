a6b9d15299120c9f512a0e6bd60021fcd33d3e68
JudeSeo <gn0408@gmail.com>
2022-08-25 21:07:08 +0900

    백준 The REAL Manhattan distance

---
+++ BackJoon/Bronze/B3/BB3_realManhattanDistance.js
@@ -0,0 +1,12 @@
+// 2022-08-25
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = [];
+input = input.slice(1)
+input.forEach(v => {
+    let [x1, y1, z1, x2, y2, z2] = v.split(" ").map(val => Number(val));
+    let dist = z1 + Math.abs(x2 - x1) + Math.abs(y2 - y1) + z2;
+    answer.push(dist)
+})
+console.log(answer.join("\n"));
