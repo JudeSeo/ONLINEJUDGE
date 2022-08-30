29d94b0d0b6fcffe6251120f4af06f7952c51936
JudeSeo <gn0408@gmail.com>
2022-08-30 18:37:02 +0900

    백준 Triangles

---
+++ BackJoon/Bronze/B3/BB3_triangles .js
@@ -0,0 +1,16 @@
+// 2022-08-30
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').map(v=>Number(v));
+let answer = [];
+input.forEach(v => {
+    let temp = [];
+    for (let i = 0; i < v; i++) {
+        for (let j = 0; j <= i;j++) {
+            temp.push("*")
+        }
+        temp.push("\n")
+    }
+    answer.push(temp.join(""))
+})
+console.log(answer.join(""));
