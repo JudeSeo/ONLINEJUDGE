3d39f0f020870f82f6b069e8551c77b388309082
JudeSeo <gn0408@gmail.com>
2022-09-03 20:34:19 +0900

    백준 알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ BackJoon/Bronze/B3/BB3_timeToRunAlgorithms2.js
@@ -0,0 +1,5 @@
+// 2022-09-03
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+console.log(input+"\n1");
