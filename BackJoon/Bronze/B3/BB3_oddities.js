d9b3b40f89a2c3acb92d1fc965321aaa45a723dc
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-09-05 04:10:55 -0700

    백준 Oddities

---
+++ BackJoon/Bronze/B3/BB3_oddities.js
@@ -0,0 +1,13 @@
+// 2022-09-05
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync('¿¹Á¦.txt').toString().trim().split('\r\n').map(v => Number(v));
+let answer = [];
+input = input.slice(1);
+input.forEach(v => {
+    if (v % 2 == 0)
+        answer.push(`${v} is even`);
+    else
+        answer.push(`${v} is odd`)
+});
+console.log(answer.join("\n"));
