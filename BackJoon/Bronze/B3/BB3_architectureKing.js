8d0ad35f132860314df974eee7c2dd8fcfebe56b
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-09-12 03:27:14 -0700

    백준 욱제는 건축왕이야!!

---
+++ BackJoon/Bronze/B3/BB3_architectureKing.js
@@ -0,0 +1,22 @@
+// 2022-09-12
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = 0;
+input = input.slice(1);
+for (let i = 0; i < input.length; i++) {
+    let aTemp;
+    let bTemp;
+    if (i == input.length - 1) {
+        aTemp = input[i].split(" ").map(v => Number(v));
+        bTemp = input[0].split(" ").map(v => Number(v));
+    } else {
+        aTemp = input[i].split(" ").map(v => Number(v));
+        bTemp = input[i + 1].split(" ").map(v => Number(v));
+    }
+    if (aTemp[0] == bTemp[0])
+        answer += Math.abs(aTemp[1] - bTemp[1])
+    else
+        answer += Math.abs(aTemp[0] - bTemp[0])
+}
+console.log(answer);
