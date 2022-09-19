3bc504dfecc6f78616c2dd49646dcaed4b2394f5
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-09-19 20:50:28 +0900

    백준 히스토그램

---
+++ BackJoon/Bronze/B3/BB3_histogram.js
@@ -0,0 +1,14 @@
+// 2022-09-19
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').map(v => Number(v));
+input = input.slice(1);
+let answer = [];
+input.forEach(v => {
+    let str = "";
+    for (let i = 0; i < v; i++) {
+        str += "="
+    }
+    answer.push(str)
+})
+console.log(answer.join("\n"));
