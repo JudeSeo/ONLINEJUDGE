af28a6e4aafc60e5cc35f5709c9e6c2abc3ae6b6
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-09-20 22:21:56 +0900

    백준 별 찍기 - 12

---
+++ BackJoon/Bronze/B3/BB3_star12.js
@@ -0,0 +1,27 @@
+// 2022-09-20
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v))[0];
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').map(v => Number(v))[0];
+let answer = [];
+for (let i = 1; i <= input; i++) {
+    let temp = "";
+    for (let k = 0; k < input - i; k++) {
+        temp += " ";
+    }
+    for (let j = 0; j < i; j++) {
+        temp += "*"
+    }
+    answer.push(temp)
+}
+for (let i = input - 1; i > 0; i--) {
+    let temp = "";
+    for (let k = 0; k < input - i; k++) {
+        temp += " ";
+    }
+    for (let j = 0; j < i; j++) {
+        temp += "*"
+    }
+    answer.push(temp)
+}
+
+console.log(answer.join("\n"));
