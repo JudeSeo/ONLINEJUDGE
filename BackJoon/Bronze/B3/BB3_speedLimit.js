e6ad48ab2f3c031d9c7728c6e3da7e5034913cc6
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-09-22 22:36:41 +0900

    백준 Speed Limit

---
+++ BackJoon/Bronze/B3/BB3_speedLimit.js
@@ -0,0 +1,19 @@
+// 2022-09-22
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = [];
+while (input.length > 1) {
+    let num = Number(input.splice(0, 1)[0]);
+    let temp = input.splice(0, Number(num));
+    let hour = 0;
+    let total = 0;
+    temp.forEach(v => {
+        let tem = v.split(" ").map(val => Number(val));
+        hour = tem[1] - hour;
+        total += tem[0] * hour;
+        hour = tem[1];
+    })
+    answer.push(`${total} miles`)
+}
+console.log(answer.join("\n"));
