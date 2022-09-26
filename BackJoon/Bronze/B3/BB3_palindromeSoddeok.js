94e10654e877865514edfe9e848e93ba89186ac4
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-09-26 21:55:44 +0900

    백준 팰린드롬 소떡소떡

---
+++ BackJoon/Bronze/B3/BB3_palindromeSoddeok.js
@@ -0,0 +1,12 @@
+// 2022-09-26
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let answer = 0;
+let num = Math.ceil(Number(input.splice(0, 1)) / 2);
+input = input[0].split("");
+for (let i = 0; i < num; i++) {
+    if (input[i] != input[input.length - i - 1])
+        answer++;
+}
+console.log(answer);
