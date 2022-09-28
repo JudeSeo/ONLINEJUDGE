66557f67993665506c61d890d555a8d698626202
JudeSeo <gn0408@gmail.com>
2022-09-28 19:24:55 +0900

    백준 Клавиатура

---
+++ BackJoon/Bronze/B3/BB3_kлавиатура.js
@@ -0,0 +1,18 @@
+// 2022-09-28
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input = input.slice(1);
+let arr = input[0].split(" ").map(v => Number(v));
+input = input.slice(2)[0].split(" ").map(v => Number(v));
+input.forEach(v => {
+    arr[v - 1]--;
+})
+arr.forEach(v => {
+    if (v < 0)
+        answer.push("yes");
+    else
+        answer.push("no")
+})
+console.log(answer.join("\n"));
