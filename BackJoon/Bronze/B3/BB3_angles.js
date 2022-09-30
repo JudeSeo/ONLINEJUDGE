86b29822b7f0fbf6245c76496ce8c73cea2090c6
JudeSeo <gn0408@gmail.com>
2022-09-30 14:33:32 +0900

    백준 Angles

---
+++ BackJoon/Bronze/B3/BB3_angles.js
@@ -0,0 +1,14 @@
+// 2022-09-30
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input = input.slice(1);
+input.forEach(v => {
+    let temp = v.split(" ").map(val => Number(val));
+    if (temp[0] + temp[1] + temp[2] != 180)
+        answer.push(v + " Check");
+    else
+        answer.push(v + " Seems OK")
+})
+console.log(answer.join("\n"))
