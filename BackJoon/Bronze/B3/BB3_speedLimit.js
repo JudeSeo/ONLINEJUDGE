f99ecdec53be3c8322e9e023cb7b8c9311a79bfe
JudeSeo <gn0408@gmail.com>
2022-10-07 16:50:20 +0900

    백준 Speed Limit

--- BackJoon/Bronze/B3/BB3_speedLimit.js
+++ BackJoon/Bronze/B3/BB3_speedLimit.js
@@ -1,7 +1,7 @@
 // 2022-09-22
 var fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
 let answer = [];
 while (input.length > 1) {
     let num = Number(input.splice(0, 1)[0]);
