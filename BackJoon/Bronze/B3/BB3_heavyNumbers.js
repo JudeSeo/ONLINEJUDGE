fac94f1035d2aaf7464c6e8f8f00bd72ba859500
JudeSeo <gn0408@gmail.com>
2022-10-10 19:20:09 +0900

    백준 Heavy Numbers

---
+++ BackJoon/Bronze/B3/BB3_heavyNumbers.js
@@ -0,0 +1,18 @@
+// 2022-10-10
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v => Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let first = input[0].toString().split("").map(v => Number(v));
+let second = input[1].toString().split("").map(v => Number(v));
+let firstAns = 0;
+let secAns = 0;
+first.forEach(v=>{
+    firstAns += v;
+})
+second.forEach(v=>{
+    secAns += v;
+})
+firstAns *= first.length;
+secAns *= second.length;
+let answer = firstAns > secAns?1:(firstAns<secAns?2:0)
+console.log(answer)
