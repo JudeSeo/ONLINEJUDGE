c3a470dc5bfb764b61affafe1d48f7030c144115
JudeSeo <gn0408@gmail.com>
2022-10-11 13:31:40 +0900

    백준 Nice Raises

---
+++ BackJoon/Bronze/B3/BB3_niceRaises.js
@@ -0,0 +1,14 @@
+// 2022-10-11
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = 0;
+let raise = input[0].split(" ")[1];
+input = input.slice(1).map(v => Number(v));
+input.forEach(v => {
+    if (v > raise)
+        answer++;
+    else if (v < raise)
+        answer--;
+})
+console.log(answer > 0 ? 2 : (answer < 0 ? 1 : 0))
