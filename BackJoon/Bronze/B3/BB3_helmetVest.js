07d13565a9da1a6469d6dce971284ec4a7f7c931
JudeSeo <gn0408@gmail.com>
2022-10-11 16:43:04 +0900

    백준 헬멧과 조끼

---
+++ BackJoon/Bronze/B3/BB3_helmetVest.js
@@ -0,0 +1,9 @@
+// 2022-10-11
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+input = input.slice(1);
+let hel = input[0].split(" ").map(v => BigInt(v)).reduce((a, b) => a < b ? b : a);
+let vest = input[1].split(" ").map(v => BigInt(v)).reduce((a, b) => a < b ? b : a);
+let answer = hel + vest;
+console.log(answer.toString())
