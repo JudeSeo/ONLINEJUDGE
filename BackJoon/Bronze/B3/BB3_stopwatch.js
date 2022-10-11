b3590fecb40df24cd4fa5581592c2c0280a39d95
JudeSeo <gn0408@gmail.com>
2022-10-11 09:42:25 +0900

    백준 Stopwatch

---
+++ BackJoon/Bronze/B3/BB3_stopwatch.js
@@ -0,0 +1,17 @@
+// 2022-10-11
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v => Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').map(v => Number(v));
+let answer = ""
+if (input[0] % 2 == 1)
+    answer = "still running";
+else {
+    let time = 0;
+    input = input.slice(1);
+    while (input.length > 0) {
+        time += (input[1] - input[0])
+        input = input.slice(2);
+    }
+    answer = time;
+}
+console.log(answer);
