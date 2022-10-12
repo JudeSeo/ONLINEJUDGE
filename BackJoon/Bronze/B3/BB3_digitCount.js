9a9bb774851da5e3e1e16b89f6959eb0f9de4eab
JudeSeo <gn0408@gmail.com>
2022-10-12 22:07:44 +0900

    백준 Digit Count

---
+++ BackJoon/Bronze/B3/BB3_digitCount.js
@@ -0,0 +1,13 @@
+// 2022-10-12
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v => Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = 0;
+for (let i = input[0]; i <= input[1]; i++) {
+    let temp = i.toString().split("")
+    temp.forEach(v => {
+        if (v == input[2])
+            answer++
+    })
+}
+console.log(answer)
