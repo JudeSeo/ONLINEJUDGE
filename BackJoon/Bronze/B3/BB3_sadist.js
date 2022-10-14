e27d735333d62a884126a795e4f991c89616839e
JudeSeo <gn0408@gmail.com>
2022-10-14 20:49:00 +0900

    백준 조교는 새디스트야!!

---
+++ BackJoon/Bronze/B3/BB3_sadist.js
@@ -0,0 +1,11 @@
+// 2022-10-14
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = 0;
+input = input.slice(1)[0].split(" ").map(v => Number(v));
+input.forEach((v, i) => {
+    if ((i + 1) != v)
+        answer++
+})
+console.log(answer)
