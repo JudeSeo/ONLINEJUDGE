d0b2654be8cb7353439fb454896303916f5feb81
JudeSeo <gn0408@gmail.com>
2022-10-18 21:50:25 +0900

    백준 고려대학교에는 공식 와인이 있다

---
+++ BackJoon/Bronze/B3/BB3_wine.js
@@ -0,0 +1,9 @@
+// 2022-10-18
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v => Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = 0;
+for (let i = 1; i <= input[0]; i++) {
+    answer += (input[1] * i + input[2] * (i ** 2))
+}
+console.log(answer);
