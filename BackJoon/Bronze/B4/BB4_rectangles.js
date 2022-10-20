dab8e8b2c86f99cb872f64c6fa9f18fec8645652
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-10-20 13:20:57 +0900

    백준 Rectangles

---
+++ BackJoon/Bronze/B4/BB4_rectangles.js
@@ -0,0 +1,12 @@
+// 2022-10-20
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync('예제.txt').toString().trim().split('\r\n').map(v => Number(v));
+let answer = "";
+for (let i = 0; i < input[0]; i++) {
+    for (let j = 0; j < input[1]; j++) {
+        answer += "*";
+    }
+    answer += "\n"
+}
+console.log(answer)
