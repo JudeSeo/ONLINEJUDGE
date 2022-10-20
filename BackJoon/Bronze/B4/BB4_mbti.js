ad063267cf93030136174066dd740acd6684c007
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-10-20 13:37:20 +0900

    백준 MBTI

---
+++ BackJoon/Bronze/B4/BB4_mbti.js
@@ -0,0 +1,24 @@
+// 2022-10-20
+const readline = require("readline");
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+let input =[];
+rl.on("line", function(line){
+    input.push(line);
+}).on("close", function(){
+    solution(input);
+    process.exit();
+});
+
+function solution(input) {
+    let j = input.splice(0, 1);
+    input = input.slice(1);
+    let answer = 0;
+    input.forEach(v => {
+        if (v == j)
+            answer++;
+    })
+    console.log(answer)
+}
