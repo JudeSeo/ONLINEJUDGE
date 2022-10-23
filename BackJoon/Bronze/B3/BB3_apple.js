e1ce8f627f5160b9e673f5de6a5d86ad3fcd13dc
JudeSeo <gn0408@gmail.com>
2022-10-23 20:24:40 +0900

    백준 사과

---
+++ BackJoon/Bronze/B3/BB3_apple.js
@@ -0,0 +1,11 @@
+// 2022-10-23
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = 0;
+input = input.slice(1);
+input.forEach(v => {
+    let temp = v.split(" ").map(val => Number(val));
+    answer += (temp[1] % temp[0])
+})
+console.log(answer)
