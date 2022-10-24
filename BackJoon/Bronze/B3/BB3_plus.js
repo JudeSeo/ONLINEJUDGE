42dff5ed1d0fa2ae48856cdd3ca2f61169f4de17
JudeSeo <gn0408@gmail.com>
2022-10-24 20:12:41 +0900

    백준 더하기

---
+++ BackJoon/Bronze/B3/BB3_plus.js
@@ -0,0 +1,16 @@
+// 2022-10-24
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input = input.slice(1);
+while (input.length > 0) {
+    input = input.slice(1);
+    let sum = 0;
+    input[0].split(" ").map(v => Number(v)).forEach(v => {
+        sum += v;
+    })
+    answer.push(sum);
+    input = input.slice(1);
+}
+console.log(answer.join("\n"))
