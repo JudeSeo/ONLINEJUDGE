c50e18cefe27efa1f8e0fec34f7d0b577946d57d
JudeSeo <gn0408@gmail.com>
2022-10-30 13:31:58 +0900

    백준 남욱이의 닭장

---
+++ BackJoon/Bronze/B3/BB3_namwokChicken.js
@@ -0,0 +1,12 @@
+// 2022-10-30
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input = input.slice(1)
+input.forEach(v => {
+    let temp = v.split(" ").map(val => Number(val));
+    let oneLeg = temp[1] * 2 - temp[0];
+    answer.push(`${oneLeg} ${temp[1] - oneLeg}`)
+})
+console.log(answer.join("\n"))
