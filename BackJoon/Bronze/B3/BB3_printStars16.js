49c51fddef9ad9d4fa0fb6db23f5ebdf999a516a
JudeSeo <gn0408@gmail.com>
2022-11-07 18:49:39 +0900

    백준 별 찍기 - 16

---
+++ BackJoon/Bronze/B3/BB3_printStars16.js
@@ -0,0 +1,16 @@
+// 2022-11-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+let answer = [];
+for (let i = 0; i < input; i++) {
+    let temp = "";
+    for (let j = input-1; j > i; j--) {
+        temp += " "
+    }
+    for (let k = 0; k <= i; k++) {
+        temp += "* ";
+    }
+    answer.push(temp.trimEnd())
+}
+console.log(answer.join("\n"));
