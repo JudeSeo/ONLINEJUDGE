f47c86113080c5010490156ee2d36466b91874d9
JudeSeo <gn0408@gmail.com>
2022-11-09 19:47:27 +0900

    백준 Window on the Wall

---
+++ BackJoon/Bronze/B3/BB3_windowOnTheWall.js
@@ -0,0 +1,8 @@
+// 2022-11-09
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v => Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let w = input[0] - input[2] * 2;
+let h = input[1] - input[2] * 2
+let answer = (w <= 0 || h <= 0) ? 0 : w * h;
+console.log(answer)
