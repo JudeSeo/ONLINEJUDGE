148afa58eda54476393e3404805853a7e0bfbf0c
JudeSeo <gn0408@gmail.com>
2022-11-11 11:21:06 +0900

    백준 Monety

---
+++ BackJoon/Bronze/B3/BB3_monety.js
@@ -0,0 +1,7 @@
+// 2022-11-11
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let num = input.splice(0, 1)[0];
+let answer = input[0].split(" ").map(v => Number(v)).filter(v => v == 1).length;
+console.log(num - answer > answer ? answer : num - answer)
