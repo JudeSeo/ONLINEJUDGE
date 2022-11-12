a9b8fecf8a28b5c43e12d4478a56250b972ed970
JudeSeo <gn0408@gmail.com>
2022-11-12 21:55:28 +0900

    골뱅이 찍기 - ㅌ

---
+++ BackJoon/Bronze/B3/BB3_seaSnailㅌ.js
@@ -0,0 +1,37 @@
+// 2022-10-07
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let input = [];
+rl.on("line", function (line) {
+    input=line
+    rl.close();
+}).on("close", function () {
+    solution(input);
+    process.exit();
+});
+const solution = (input) => {
+    let answer = [];
+    for (let i = 0; i < 5; i++) {
+        let temp = "";
+        if (i == 0 || i == 2 || i==4) {
+            for (let j = 0; j < input; j++) {
+                for (let k = 0; k < 5 * input; k++) {
+                    temp += "@";
+                }
+                temp += "\n";
+            }
+        } else {
+            for (let j = 0; j < input; j++) {
+                for (let k = 0; k < input; k++) {
+                    temp += "@";
+                }
+                temp += "\n";
+            }
+        }
+        answer.push(temp)
+    }
+    console.log(answer.join(""))
+}
