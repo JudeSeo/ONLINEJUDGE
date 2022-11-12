e9e8064e158b5b70a05a8579534c1b3afcdee9cc
JudeSeo <gn0408@gmail.com>
2022-11-12 21:57:04 +0900

    백준 골뱅이 찍기 - 뒤집힌 ㅋ

--- BackJoon/Bronze/B3/BB3_seaSnailㅋ.js
+++ BackJoon/Bronze/B3/BB3_seaSnailㅋ.js
@@ -1,29 +1,37 @@
-// 2022-10-07
-var fs = require('fs');
-// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
-var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
-input = Number(input)
-let answer = [];
-for (let i = 0; i < 5; i++) {
-    let temp = "";
-    if (i == 0 || i == 2) {
-        for (let j = 0; j < input; j++) {
-            for (let k = 0; k < 5 * input; k++) {
-                temp += "@";
+// 2022-10-07 => 2022-11-12
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let input = [];
+rl.on("line", function (line) {
+    input=line
+    rl.close();
+}).on("close", function () {
+    solution(input);
+    process.exit();
+});
+const solution = (input) => {
+    let answer = [];
+    for (let i = 0; i < 5; i++) {
+        let temp = "";
+        if (i == 0 || i == 2) {
+            for (let j = 0; j < input; j++) {
+                for (let k = 0; k < 5 * input; k++) {
+                    temp += "@";
+                }
+                temp += "\n";
             }
-            temp += "\n";
+        } else {
+            for (let j = 0; j < input; j++) {
+                for (let k = 0; k < input; k++) {
+                    temp += "@";
+                }
+                temp += "\n";
+            }
         }
-    } else {
-        for (let j = 0; j < input; j++) {
-            for (let k = 0; k < input; k++) {
-                temp += "@";
-            }
-            for (let k = 0; k < 4 * input; k++) {
-                temp += " ";
-            }
-            temp += "\n";
-        }
+        answer.push(temp)
     }
-    answer.push(temp)
+    console.log(answer.join("").trim())
 }
-console.log(answer.join("").trim())
