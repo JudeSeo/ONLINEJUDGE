3e344d1c24c0c9af00a1b5e7d3acaa8341ed3a50
JudeSeo <gn0408@gmail.com>
2022-11-15 19:33:42 +0900

    백준 골뱅이 찍기 - 정사각형

---
+++ BackJoon/Bronze/B3/BB3_seaSnailSquare.js
@@ -0,0 +1,28 @@
+// 2022-11-15
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+input = Number(input)
+let answer = [];
+for (let i = 0; i < input + 2; i++) {
+    let temp = "";
+    if (i == 0 || i == input + 1) {
+        for (let k = 0; k < input + 2; k++) {
+            temp += "@";
+        }
+        temp += "\n";
+    } else {
+        for (let k = 0; k < 1; k++) {
+            temp += "@";
+        }
+        for (let k = 0; k < input; k++) {
+            temp += " ";
+        }
+        for (let k = 0; k < 1; k++) {
+            temp += "@";
+        }
+        temp += "\n";
+    }
+    answer.push(temp)
+}
+console.log(answer.join(""))
