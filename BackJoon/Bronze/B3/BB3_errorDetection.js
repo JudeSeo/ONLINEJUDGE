ffad6cb6c0e8a132e49936bfa615fca8ac7046d3
JudeSeo <gn0408@gmail.com>
2022-11-23 20:12:55 +0900

    백준 Error Detection

---
+++ BackJoon/Bronze/B3/BB3_errorDetection.js
@@ -0,0 +1,16 @@
+// 2022-11-23
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input = input.slice(1);
+input.forEach(v => {
+    let temp = v.split(" ").map(val => Number(val));
+    let bi = temp[0].toString(2).split("").filter(val => val == 1).length % 2;
+    if (bi == temp[1])
+        answer.push("Valid")
+    else
+        answer.push("Corrupt")
+
+})
+console.log(answer.join("\n"));
