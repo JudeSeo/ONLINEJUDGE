0086b988cc6f72240ab43efc4a4ae6e28eb5179c
JudeSeo <gn0408@gmail.com>
2022-11-24 22:56:24 +0900

    백준 Handebol

---
+++ BackJoon/Bronze/B3/BB3_handebol.js
@@ -0,0 +1,12 @@
+// 2022-11-24
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = 0;
+input = input.slice(1);
+input.forEach(v => {
+    let temp = v.split(" ").map(val => Number(val));
+    if (temp.indexOf(0) == -1)
+        answer++;
+})
+console.log(answer)
