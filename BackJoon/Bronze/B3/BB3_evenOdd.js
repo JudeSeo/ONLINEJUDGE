61a93924cb7a6331af8a59564869a2015ba44135
JudeSeo <gn0408@gmail.com>
2022-11-25 19:05:10 +0900

    백준 Even or Odd?

---
+++ BackJoon/Bronze/B3/BB3_evenOdd.js
@@ -0,0 +1,14 @@
+// 2022-11-25
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v))[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').map(v => Number(v))[0];
+let answer = 0;
+if (input % 2 == 1)
+    answer = 0;
+else {
+    if (input % 4 == 0)
+        answer = 2
+    else
+        answer = 1;
+}
+console.log(answer);
