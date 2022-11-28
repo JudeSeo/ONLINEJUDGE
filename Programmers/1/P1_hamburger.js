ce426aab412a439ecb9797a8aed5883a2d560c85
JudeSeo <gn0408@gmail.com>
2022-11-28 19:34:42 +0900

    프로그래머스 햄버거 만들기

---
+++ Programmers/1/P1_hamburger.js
@@ -0,0 +1,28 @@
+// 2022-11-28
+function solution(ingredient) {
+    var answer = 0;
+    let stack = [];
+    for (let i = 0; i < ingredient.length; i++) {
+        stack.push(ingredient[i]);
+        if (stack.length < 4)
+            continue;
+        if (stack[stack.length - 4] == 1 &&
+            stack[stack.length - 3] == 2 &&
+            stack[stack.length - 2] == 3 &&
+            stack[stack.length - 1] == 1
+        ) {
+            answer++
+            stack.pop();
+            stack.pop();
+            stack.pop();
+            stack.pop();
+        }
+    }
+    console.log(answer)
+    return answer;
+}
+
+solution([2, 1, 1, 2, 3, 1, 2, 3, 1]); //2
+
+
+// 왜 스택으로 풀어야하는지 모르겠다.
