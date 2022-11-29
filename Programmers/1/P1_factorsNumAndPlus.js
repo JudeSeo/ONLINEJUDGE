37ce90faf077cee65ede9c0f932c7a3b066c4ee2
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-11-29 11:33:37 +0900

    프로그래머스 약수의 개수와 덧셈

---
+++ Programmers/1/P1_factorsNumAndPlus.js
@@ -0,0 +1,17 @@
+// 2022-11-29
+function solution(left, right) {
+    var answer = 0;
+    for (let i = left; i <= right; i++) {
+        let temp = 0;
+        for (let j = 1; j <= i; j++) {
+            if (i % j == 0) temp++;
+        }
+        if (temp % 2 == 0) answer += i;
+        else answer -= i;
+    }
+    console.log(answer)
+    return answer;
+}
+
+solution(13, 17);
+solution(24, 27);
