f9525a83ffafb94a6524070183e91895bb302ab5
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-11-30 14:52:11 +0900

    백준 Coffee Cup Combo

---
+++ BackJoon/Bronze/B3/BB3_coffeeCupCombo.js
@@ -0,0 +1,17 @@
+// 2022-11-30
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n');
+let answer = 0;
+input = input.slice(1)[0].split("").map(v => Number(v));
+let coffee = 0;
+input.forEach(v => {
+    if (v == 1) {
+        answer++
+        coffee = 2;
+    } else if (coffee > 0) {
+        coffee--;
+        answer++;
+    }
+})
+console.log(answer)
