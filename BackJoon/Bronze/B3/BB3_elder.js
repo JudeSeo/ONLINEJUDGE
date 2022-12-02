1ed5e38fa34c1386e316f0b553f4c5dd36691df9
JudeSeo <gn0408@gmail.com>
2022-12-02 22:51:47 +0900

    백준 Elder

---
+++ BackJoon/Bronze/B3/BB3_elder.js
@@ -0,0 +1,17 @@
+// 2022-12-02
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let master = input[0];
+let answer = new Set();
+answer.add(master)
+input = input.slice(2)
+input.forEach(v => {
+    let temp = v.split(" ")
+    if (temp[1] == master) {
+        master = temp[0];
+        answer.add(temp[0])
+    }
+})
+console.log(master);
+console.log(answer.size);
