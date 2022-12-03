3f4b55d39e48ac1f5165228fcf4deee1e5ed1afd
JudeSeo <gn0408@gmail.com>
2022-12-03 20:34:15 +0900

    백준 A Serious Reading Problem

---
+++ BackJoon/Bronze/B3/BB3_seriousReadingProblem.js
@@ -0,0 +1,11 @@
+// 2022-12-03
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input.forEach(v => {
+    if (v == "0 0 0 0") return;
+    let temp = v.split(" ").map(val => Number(val));
+    answer.push(Math.pow(Math.pow(Math.pow(temp[0], temp[1]), temp[2]), temp[3]))
+})
+console.log(answer.join("\n"))
