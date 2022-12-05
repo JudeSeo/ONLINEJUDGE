2fc4e63856bee3c7a14c1bb0ab3107e6e8ae801b
JudeSeo <gn0408@gmail.com>
2022-12-05 20:09:32 +0900

    백준 Right Triangle

---
+++ BackJoon/Bronze/B3/BB3_rightTriangle.js
@@ -0,0 +1,16 @@
+// 2022-12-05
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+input = input.slice(1);
+let answer = [];
+input.forEach((v, i) => {
+    let temp = v.split(" ").map(val => Number(val)).sort((a, b) => b-a);
+    let str = `Case #${i + 1}: `
+    if (temp[0] ** 2 == temp[1] ** 2 + temp[2] ** 2)
+        str += "YES"
+    else
+        str += "NO"
+    answer.push(str)
+})
+console.log(answer.join("\n"))
