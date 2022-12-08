8c7b73d88529e59caf13c37e710009b492b1b389
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2022-12-08 11:17:05 +0900

    백준 Gift

---
+++ BackJoon/Bronze/B3/BB3_gift.js
@@ -0,0 +1,11 @@
+// 2022-12-08
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n');
+let [num, mi] = input.splice(0, 1)[0].split(" ").map(v => Number(v));
+let answer = 0;
+for (let i = 0; i < num - 1; i++) {
+    if (input[i] - input[i + 1] >= mi)
+        answer++
+}
+console.log(answer)
