1a447441dff71fcabbe7694363ba37d3ef0a9a03
JudeSeo <gn0408@gmail.com>
2022-12-14 18:45:57 +0900

    백준 공

---
+++ BackJoon/Bronze/B3/BB3_ball.js
@@ -0,0 +1,14 @@
+// 2022-12-14
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = 1;
+input = input.slice(1);
+input.forEach(v => {
+    let temp = v.split(" ")
+    if (temp[0] == answer)
+        answer = temp[1]
+    else if (temp[1] == answer)
+        answer = temp[0]
+})
+console.log(answer)
