594d1e24efc36f169415a201fbbb34dc61bff704
JudeSeo <gn0408@gmail.com>
2022-12-21 19:29:17 +0900

    백준 세탁소 사장 동혁

---
+++ BackJoon/Bronze/B3/BB3_laundryShop.js
@@ -0,0 +1,17 @@
+// 2022-12-21
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1).map(v=>Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1).map(v => Number(v));
+let answer = [];
+input.forEach(v => {
+    let change = [];
+    change.push(Math.floor(v / 25))
+    v = v % 25;
+    change.push(Math.floor(v / 10))
+    v = v % 10;
+    change.push(Math.floor(v / 5))
+    v = v % 5;
+    change.push(Math.floor(v))
+    answer.push(change.join(" "));
+})
+console.log(answer.join("\n"))
