c2d0fd00c18e93e3bd70e4011f42393a2ce33522
JudeSeo <gn0408@gmail.com>
2022-12-27 20:24:00 +0900

    백준 최댓값

---
+++ BackJoon/Bronze/B3/BB3_biggestNum.js
@@ -0,0 +1,16 @@
+// 2022-12-27
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+let max = 0, line = 0;
+input.forEach((v, i) => {
+    let temp = v.split(" ").map(val => Number(val));
+    if (Math.max(...temp) > max) {
+        max = Math.max(...temp);
+        line = i;
+    }
+})
+answer.push(line + 1, input[line].split(" ").map(v => Number(v)).indexOf(max) + 1);
+console.log(max)
+console.log(answer.join(" "))
