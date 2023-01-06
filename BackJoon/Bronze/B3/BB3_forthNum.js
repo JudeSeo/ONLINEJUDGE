24858ef832278cd7bd52ad4d30a25a4ba36c89fa
JudeSeo <gn0408@gmail.com>
2023-01-06 19:05:17 +0900

    백준 네 번째 수

---
+++ BackJoon/Bronze/B3/BB3_forthNum.js
@@ -0,0 +1,11 @@
+// 2023-01-06
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v)).sort((a, b)=>a-b);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v)).sort((a, b) => a - b);
+let answer = 0, num = Math.min(input[2] - input[1], input[1] - input[0]);
+if (input[0] + num != input[1])
+    answer = input[0] + num;
+else if (input[1] + num != input[2])
+    answer = input[1] + num;
+else answer = input[2] + num;
+console.log(answer)
