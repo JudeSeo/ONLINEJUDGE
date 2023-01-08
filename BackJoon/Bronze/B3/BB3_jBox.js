3c1b2d876c06f07587b2fccd8dfa6e1b311a9aae
JudeSeo <gn0408@gmail.com>
2023-01-08 21:53:13 +0900

    백준 J박스

---
+++ BackJoon/Bronze/B3/BB3_jBox.js
@@ -0,0 +1,27 @@
+// 2023-01-08
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1).map(v=>Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1).map(v => Number(v));
+let answer = [];
+input.forEach(v => {
+    let temp = "";
+    for (let i = 0; i < v; i++) {
+        for (let j = 0; j < v; j++) {
+            if (i == 0)
+                temp += "#";
+            else if (i == v - 1)
+                temp += "#";
+            else {
+                if (j == 0)
+                    temp += "#";
+                else if (j == v - 1)
+                    temp += "#";
+                else
+                    temp += "J";
+            }
+        }
+        temp += "\n";
+    }
+    answer.push(temp)
+})
+console.log(answer.join("\n"))
