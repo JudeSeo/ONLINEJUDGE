b53574381af1dc2e8496e75a12e3c793ed43412e
JudeSeo <gn0408@gmail.com>
2023-01-09 19:18:01 +0900

    백준 No Brainer

---
+++ BackJoon/Bronze/B3/BB3_noBrainer.js
@@ -0,0 +1,13 @@
+// 2023-01-09
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach(v => {
+    let temp = v.split(" ").map(val => Number(val));
+    if (temp[0] >= temp[1])
+        answer.push("MMM BRAINS");
+    else
+        answer.push("NO BRAINS");
+})
+console.log(answer.join("\n"))
