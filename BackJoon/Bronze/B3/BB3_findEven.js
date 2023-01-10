05aa79b0768dcf7248683e226bcc8b1d1dabd929
JudeSeo <gn0408@gmail.com>
2023-01-10 20:22:21 +0900

    백준 짝수를 찾아라

---
+++ BackJoon/Bronze/B3/BB3_findEven.js
@@ -0,0 +1,12 @@
+// 2023-01-10
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach(v => {
+    let arr = 0;
+    let temp = v.split(" ").map(val => Number(val)).filter(val => val % 2 == 0);
+    temp.map(val => arr += val)
+    answer.push(`${arr} ${Math.min(...temp)}`);
+})
+console.log(answer.join("\n"))
