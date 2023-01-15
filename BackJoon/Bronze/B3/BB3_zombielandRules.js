87e7127c25aed517f3a42241a57399e62c15f3e8
JudeSeo <gn0408@gmail.com>
2023-01-15 21:10:54 +0900

    백준 Always Follow the Rules in Zombieland

---
+++ BackJoon/Bronze/B3/BB3_zombielandRules.js
@@ -0,0 +1,11 @@
+// 2023-01-15
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+let rules = input.slice(1, Number(input[0]) + 1)
+input = input.slice(Number(input[0]) + 2).map(v => Number(v))
+input.forEach(v => {
+    answer.push(`Rule ${v}: ${rules[v - 1] == undefined?'No such rule':rules[v - 1]}`)
+})
+console.log(answer.join("\n"))
