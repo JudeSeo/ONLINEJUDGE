3d613aa47aa2048c421d7ed13036f2f87d58fb98
JudeSeo <gn0408@gmail.com>
2023-01-17 19:09:18 +0900

    백준 ABC

---
+++ BackJoon/Bronze/B3/BB3_abc.js
@@ -0,0 +1,16 @@
+// 2023-01-17
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+let abc = input[1].split("");
+let num = input[0].split(" ").map(v => Number(v)).sort((a, b) => a - b);
+abc.forEach(v => {
+    if (v == "A")
+        answer.push(num[0]);
+    else if (v == "B")
+        answer.push(num[1]);
+    else if (v == "C")
+        answer.push(num[2]);
+})
+console.log(answer.join(" "))
