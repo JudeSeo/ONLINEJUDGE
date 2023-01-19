8b128572ea6831ab28089e0dc962644e123e4bc8
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-01-19 15:12:04 +0900

    백준 Hot Dogs

---
+++ BackJoon/Bronze/B3/BB3_hotDogs.js
@@ -0,0 +1,10 @@
+// 2023-01-19
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n');
+let answer=[];
+input.forEach(v=>{
+    let temp = v.split(" ").map(v=>Number(v));
+    answer.push((temp[0]/temp[1]).toFixed(2))
+})
+console.log(answer.join("\n"));
