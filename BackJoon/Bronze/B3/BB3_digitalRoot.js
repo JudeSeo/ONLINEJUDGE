c31c9bc1074ddba5974fdb2388a5c154e4521a98
JudeSeo <gn0408@gmail.com>
2023-01-29 21:15:55 +0900

    백준 디지털 루트

---
+++ BackJoon/Bronze/B3/BB3_digitalRoot.js
@@ -0,0 +1,17 @@
+// 2023-01-29
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input.forEach(v => {
+    if (v == "0") return
+    let sum = 0
+    v.split("").map(val => sum += Number(val));
+    while (parseInt(sum / 10) != 0) {
+        let tempSum = 0
+        sum.toString().split("").map(val => tempSum += Number(val));
+        sum = tempSum;
+    }
+    answer.push(sum)
+})
+console.log(answer.join("\n"))
