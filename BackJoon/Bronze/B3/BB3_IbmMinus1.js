97f4dcde9aa48e5baba9cba6c3cc4a7840a93fe5
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-01-31 11:28:48 +0900

    백준 IBM 빼기 1

---
+++ BackJoon/Bronze/B3/BB3_IbmMinus1.js
@@ -0,0 +1,15 @@
+// 2023-01-31
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach((v, i) => {
+    let temp = v.split("").map(val => {
+        val = String.fromCharCode(val.charCodeAt() + 1)
+        if (val.charCodeAt() == 91)
+            val = "A";
+        return val
+    })
+    answer.push(`String #${i + 1}\n${temp.join("")}`)
+})
+console.log(answer.join("\n\n"))
