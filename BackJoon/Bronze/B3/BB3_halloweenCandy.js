d0548b02eaf21cf66b999800bbc7e15c76576921
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-01-31 13:34:11 +0900

    백준 할로윈의 사탕

---
+++ BackJoon/Bronze/B3/BB3_halloweenCandy.js
@@ -0,0 +1,10 @@
+// 2023-01-31
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach((v, i) => {
+    let temp = v.split(" ").map(val=>Number(val));
+    answer.push(`You get ${Math.floor(temp[0]/temp[1])} piece(s) and your dad gets ${temp[0]%temp[1]} piece(s).`)
+})
+console.log(answer.join("\n"))
