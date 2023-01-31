bfc937f96bee82c9e541a958e3077b0b60a8df44
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-01-31 13:19:17 +0900

    백준 얼마?

---
+++ BackJoon/Bronze/B3/BB3_howMuch.js
@@ -0,0 +1,16 @@
+// 2023-01-31
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+while (input.length > 0) {
+    let price = Number(input.splice(0, 1)[0]);
+    let num = input.splice(0, 1);
+    let option = input.splice(0, num);
+    option.forEach(v => {
+        let temp = v.split(" ").map(val => Number(val));
+        price += temp[0] * temp[1];
+    })
+    answer.push(price);
+}
+console.log(answer.join("\n"))
