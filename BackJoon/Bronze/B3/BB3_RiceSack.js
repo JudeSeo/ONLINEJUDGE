279fbea0f2a3aaf0adb70863dc679e5dcd4aea8c
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-01-31 13:26:15 +0900

    백준 RICE SACK

---
+++ BackJoon/Bronze/B3/BB3_RiceSack.js
@@ -0,0 +1,10 @@
+// 2023-01-31
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach((v, i) => {
+    let temp = v.split(" ").map(val => Number(val));
+    answer.push(`Case #${i + 1}: ${Math.max(...temp)}`)
+})
+console.log(answer.join("\n"))
