aab295000a62a5ff703836568bb7d9e6a62b1be9
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-02-01 16:53:33 +0900

    백준 Test

---
+++ BackJoon/Bronze/B3/BB3_test.js
@@ -0,0 +1,10 @@
+// 2023-02-01
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1).map(v => BigInt(v));
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n').slice(1).map(v => BigInt(v));
+let answerList = ['A', 'B', 'C', 'B', 'C', 'D', 'C', 'D', 'A', 'D', 'A', 'B']
+let answer = [];
+input.forEach(v => {
+    answer.push(answerList[(v - 1) % 12]);
+})
+console.log(answer.join("\n"));
