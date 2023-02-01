0a2c1904d7c29646ed5e900b39a3a08ad7906375
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-02-01 16:42:54 +0900

    백준 아이 러브 크로아티아

---
+++ BackJoon/Bronze/B3/BB3_iLoveCroatia.js
@@ -0,0 +1,15 @@
+// 2023-02-01
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n');
+let k = Number(input.splice(0, 1)[0]);
+input = input.slice(1);
+let score = 0;
+input.forEach(v => {
+    let [t, z] = v.split(" ");
+    score += Number(t);
+    if (score >= 210) return
+    if (z == "T") k++;
+    if (k > 8) k -= 8;
+})
+console.log(k);
