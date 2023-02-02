33b924f1844e311e3183bea00ad2af66f502c6b5
JudeSeo <gn0408@gmail.com>
2023-02-02 20:43:21 +0900

    백준 택시 기하학

---
+++ BackJoon/Bronze/B3/BB3_taxiGeometry.js
@@ -0,0 +1,6 @@
+// 2023-02-02
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+console.log((input ** 2 * Math.PI).toFixed(6))
+console.log((input ** 2 * 2).toFixed(6))
