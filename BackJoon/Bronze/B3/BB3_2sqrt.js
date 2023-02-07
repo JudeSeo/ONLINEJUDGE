a5fc4e9259b4b253f7aa94e139fcf9e6e6a27104
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-02-07 09:48:33 +0900

    백준 2의 제곱인가?

---
+++ BackJoon/Bronze/B3/BB3_2sqrt.js
@@ -0,0 +1,11 @@
+// 2023-02-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n')[0];
+let answer = 0
+if (Math.sqrt(input) == parseInt(Math.sqrt(input)))
+    answer = 1;
+else if (Math.sqrt(input * 2) == parseInt(Math.sqrt(input * 2))) {
+    answer = 1;
+}
+console.log(answer);
