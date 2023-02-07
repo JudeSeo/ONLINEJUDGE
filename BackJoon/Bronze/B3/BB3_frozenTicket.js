e84a74ea5d551a4a265ed1aae67e21ad14e8bc3e
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-02-07 10:30:38 +0900

    백준 겨울왕국 티켓 예매

---
+++ BackJoon/Bronze/B3/BB3_frozenTicket.js
@@ -0,0 +1,15 @@
+// 2023-02-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1;
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+while (input.length > 0) {
+    let temp = input.splice(0, 1)[0].split(" ").map(v => Number(v));
+    if (temp[0] < 12)
+        answer.push(-1);
+    else if (temp[1] < 4)
+        answer.push(-1)
+    else
+        answer.push(temp[1] * 11 + 4);
+}
+console.log(answer.join("\n"));
