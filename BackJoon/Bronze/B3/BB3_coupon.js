65859811e85dea920bc2ac7deaded661428916dc
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-02-07 09:58:42 +0900

    백준 쿠폰

---
+++ BackJoon/Bronze/B3/BB3_coupon.js
@@ -0,0 +1,9 @@
+// 2023-02-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1).map(v=>Number(v));
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n').slice(1).map(v => Number(v));
+let answer = [];
+input.forEach(v => {
+    answer.push("$" + (v * 0.8).toFixed(2))
+})
+console.log(answer.join("\n"));
