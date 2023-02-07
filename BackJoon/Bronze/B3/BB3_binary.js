5f5a04f135c9fc8da244215fabc207b18c4b66f1
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-02-07 09:56:28 +0900

    백준 이진수

---
+++ BackJoon/Bronze/B3/BB3_binary.js
@@ -0,0 +1,14 @@
+// 2023-02-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1).map(v=>Number(v));
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n').slice(1).map(v => Number(v));
+let answer = [];
+while (input.length > 0) {
+    let temp = input.splice(0, 1)[0].toString(2).split("").reverse();
+    let arr = [];
+    temp.forEach((v, i) => {
+        if (v == 1) arr.push(i)
+    })
+    answer.push(arr.join(" "))
+}
+console.log(answer.join("\n"));
