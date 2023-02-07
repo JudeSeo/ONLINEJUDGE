5d8e612bd7234feed298f82716cba1046e227095
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-02-07 09:41:38 +0900

    백준 Baseball

---
+++ BackJoon/Bronze/B3/BB3_baseball.js
@@ -0,0 +1,16 @@
+// 2023-02-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = []
+while (input.length > 1) {
+    let y = 0, k = 0;
+    let temp = input.splice(0, 9)
+    temp.forEach(v => {
+        let score = v.split(" ").map(val => Number(val));
+        y += score[0];
+        k += score[1];
+    })
+    answer.push(y > k ? "Yonsei" : y < k ? "Korea" : "Draw");
+}
+console.log(answer.join("\n"));
