5b2d1d5beb6894038b59e3c3f073df3aaf6e36be
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-02-13 09:13:55 +0900

    백준 최소, 최대 2

---
+++ BackJoon/Bronze/B3/BB3_minMax2.js
@@ -0,0 +1,10 @@
+// 2023-02-13
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+while (input.length > 0) {
+    let temp = input.splice(0, 2)[1].split(" ").map(v => Number(v));
+    answer.push(Math.min(...temp) + " " + Math.max(...temp))
+}
+console.log(answer.join("\n"));
