27c380a41f9c1c4ce6a53794528afc606f85b83d
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-02-17 16:37:49 +0900

    백준 일우는 야바위꾼

---
+++ BackJoon/Bronze/B3/BB3_wildRocker.js
@@ -0,0 +1,14 @@
+// 2023-02-17
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n');
+let [n, x, k] = input.splice(0, 1)[0].split(" ").map(v => Number(v));
+let answer = x;
+input.forEach(v => {
+    let temp = v.split(" ");
+    if (temp[0] == answer)
+        answer = temp[1];
+    else if (temp[1] == answer)
+        answer = temp[0];
+})
+console.log(answer);
