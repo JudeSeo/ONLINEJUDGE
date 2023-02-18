a2b747c372897ae7c85e0911ce8595980a50c14d
JudeSeo <gn0408@gmail.com>
2023-02-18 23:18:57 +0900

    백준 신기한 수

---
+++ BackJoon/Bronze/B3/BB3_amazingNum.js
@@ -0,0 +1,16 @@
+// 2023-02-18
+console.log(new Date())
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+let answer = 0;
+for (let i = 1; i <= input; i++) {
+    let temp = i, sum = 0;
+    while (temp >= 1) {
+        sum += temp % 10
+        temp = parseInt(temp / 10);
+    }
+    if (i % sum == 0) answer++;
+}
+console.log(answer);
+console.log(new Date())
