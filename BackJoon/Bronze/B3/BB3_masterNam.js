f3fd9d2a4f4c1c152e51532adf156436dfbadb59
JudeSeo <gn0408@gmail.com>
2023-02-26 21:23:51 +0900

    백준 명장 남정훈

---
+++ BackJoon/Bronze/B3/BB3_masterNam.js
@@ -0,0 +1,8 @@
+// 2023-02-26
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = Math.min(input[0] + input[2], input[1] + input[2], input[0] + input[1] + input[2] >> 1) * 2
+console.log(answer);
+
+// 내가 푼건 아니지만 시프트연산을 사용해서 신기했다.
