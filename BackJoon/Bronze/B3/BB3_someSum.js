1d9250478e8e997ae036702014112eb5ecc4233f
JudeSeo <gn0408@gmail.com>
2023-03-01 11:50:29 +0900

    백준 Some Sum

---
+++ BackJoon/Bronze/B3/BB3_someSum.js
@@ -0,0 +1,9 @@
+// 2023-03-01
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+let answer = "";
+if (input % 4 == 0) answer = "Even";
+else if (input % 2 == 0) answer = "Odd";
+else answer = "Either";
+console.log(answer)
