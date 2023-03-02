6dffc7dff481dcc30b852d1ea7d9698844495f3e
JudeSeo <gn0408@gmail.com>
2023-03-02 22:53:11 +0900

    백준 등차수열에서 항 번호 찾기

---
+++ BackJoon/Bronze/B3/BB3_arithmeticProgression.js
@@ -0,0 +1,12 @@
+// 2023-03-02
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = 0;
+if ((input[2] - input[0]) / input[1] < 0)
+    answer = "X"
+else if ((input[2] - input[0]) / input[1] == parseInt((input[2] - input[0]) / input[1]))
+    answer = (input[2] - input[0]) / input[1] + 1
+else
+    answer = "X"
+console.log(answer)
