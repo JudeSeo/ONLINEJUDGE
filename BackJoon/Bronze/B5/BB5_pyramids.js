391b2c8492b418d8aff5ce8b2fc8fd1cc908ead2
JudeSeo <gn0408@gmail.com>
2023-03-04 23:57:19 +0900

    백준 Pyramids

---
+++ BackJoon/Bronze/B5/BB5_pyramids.js
@@ -0,0 +1,15 @@
+// 2023-03-04
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+let sum = 0
+input.forEach(v => {
+    if (v == "0") return;
+    let sum = 0;
+    for (let i = 1; i <= v; i++) {
+        sum += i
+    }
+    answer.push(sum)
+})
+console.log(answer.join("\n"))
