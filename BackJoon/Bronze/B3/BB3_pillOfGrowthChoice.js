a759a9d4c23046639f7e423d2aea2d2e5e9aeb72
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-03-07 11:12:25 +0900

    백준 성장의 비약 선택권

---
+++ BackJoon/Bronze/B3/BB3_pillOfGrowthChoice.js
@@ -0,0 +1,14 @@
+// 2023-03-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n')[0];
+let answer = 0
+if (input >= 200 && input < 206)
+    answer = 1;
+else if (input >= 206 && input < 218)
+    answer = 2;
+else if (input >= 218 && input < 229)
+    answer = 3;
+else
+    answer = 4;
+console.log(answer)
