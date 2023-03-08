2152051ba3c8787617360ad7c16a124213b8a1ef
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-03-08 13:27:33 +0900

    백준 Vowel Count

---
+++ BackJoon/Bronze/B3/BB3_vowelCount.js
@@ -0,0 +1,12 @@
+// 2023-03-08
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+let vowel = ['a', 'e', 'i', 'o', 'u']
+input.forEach(v => {
+    let vo = 0, cons = 0;
+    v.split("").map(val => vowel.includes(val) ? vo++ : cons++)
+    answer.push(vo > cons ? v + "\n1" : v + "\n0");
+})
+console.log(answer.join("\n"))
