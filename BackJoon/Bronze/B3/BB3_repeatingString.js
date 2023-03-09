b37cf57d9e6855b0c31479455185f7215e8ddfe5
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-03-09 15:01:00 +0900

    백준 繰り返し文字列 (Repeating String)

---
+++ BackJoon/Bronze/B3/BB3_repeatingString.js
@@ -0,0 +1,8 @@
+// 2023-03-09
+var fs = require('fs');
+// let [num, input] = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+var [num, input] = fs.readFileSync("예제.txt").toString().trim().split('\r\n')
+let first = input.split("").splice(0, num / 2).join("");
+let second = input.split("").splice(num / 2).join("");
+let answer = first == second ? "Yes" : "No";
+console.log(answer)
