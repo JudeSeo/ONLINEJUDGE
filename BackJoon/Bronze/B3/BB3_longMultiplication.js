beedb21a08d420bc4d93c6d19e1e6c49803f54e1
JudeSeo <gn0408@gmail.com>
2023-03-12 12:25:35 +0900

    백준 Long Multiplication

---
+++ BackJoon/Bronze/B3/BB3_longMultiplication.js
@@ -0,0 +1,13 @@
+// 2023-03-12
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>BigInt(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').map(v => BigInt(v));
+let answer = input.slice(0);
+let temp = input[1].toString().split("").map(v => BigInt(v));
+temp.reverse().forEach(v => {
+    answer.push(input[0] * v)
+})
+answer.push(input[0] * input[1])
+console.log(answer.join("\n"))
+
+// 왜 틀렸나 했는데 범위 초과... BigInt로 수정하니까 맞았다.
