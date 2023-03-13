eb8c5c47b3af226b7fa8ad951a5de8088c65220d
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-03-13 10:46:57 +0900

    백준 Mult!

---
+++ BackJoon/Bronze/B3/BB3_mult.js
@@ -0,0 +1,14 @@
+// 2023-03-13
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1).map(v => Number(v));
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n').slice(1).map(v => Number(v));
+let answer = [];
+let num = input[0];
+for (let i = 1; i < input.length; i++) {
+    if (input[i] % num == 0) {
+        answer.push(input[i])
+        i++;
+        num = input[i];
+    }
+}
+console.log(answer.join("\n"))
