f60163f447efded9f0308a9f70338ccc5ef9cf6d
JudeSeo <gn0408@gmail.com>
2023-03-21 22:40:09 +0900

    백준 헤라클레스와 히드라

---
+++ BackJoon/Bronze/B3/BB3_heraclesHydra.js
@@ -0,0 +1,17 @@
+// 2023-03-21
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+let index = 0;
+while (input.length > 0) {
+    index++;
+    let num = input.splice(0, 1)[0];
+    let temp = input.splice(0, 1)[0].split("");
+    temp.forEach(v => {
+        if (v == "c") num++;
+        else num--;
+    })
+    answer.push(`Data Set ${index}:\n${num}`);
+}
+console.log(answer.join("\n\n"))
