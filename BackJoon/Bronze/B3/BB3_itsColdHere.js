80e041c266069f8a4a8cdb875fa1e8e05ed745ec
JudeSeo <gn0408@gmail.com>
2023-03-22 21:35:48 +0900

    백준 It’s Cold Here!

---
+++ BackJoon/Bronze/B3/BB3_itsColdHere.js
@@ -0,0 +1,14 @@
+// 2023-03-22
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = "";
+let temp = 200;
+input.forEach(v => {
+    let tem = v.split(" ");
+    if (temp > Number(tem[1])) {
+        temp = Number(tem[1])
+        answer = tem[0]
+    }
+})
+console.log(answer);
