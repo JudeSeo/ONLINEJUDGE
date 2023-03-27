868256a7c0e602fe382f8a8a161f085b9a650be8
JudeSeo <gn0408@gmail.com>
2023-03-27 22:31:37 +0900

    백준 Overdraft

---
+++ BackJoon/Bronze/B3/BB3_overdraft.js
@@ -0,0 +1,11 @@
+// 2023-03-27
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1).map(v=>BigInt(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1).map(v => BigInt(v));
+let answer = 0n;
+let acc = 0n;
+input.forEach((v, i) => {
+    acc += v;
+    answer = answer < acc ? answer : acc;
+})
+console.log((answer * -1n).toString());
