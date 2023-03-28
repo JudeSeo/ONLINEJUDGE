8bcd91fb208f0d0539af586dae78a379b6f0bf63
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-03-28 11:37:51 +0900

    백준 Not A + B

---
+++ BackJoon/Bronze/B3/BB3_notA+B.js
@@ -0,0 +1,7 @@
+// 2023-03-28
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n')[0];
+for (let i = 0; i < input; i++) {
+    console.log(1 + "\n");
+}
