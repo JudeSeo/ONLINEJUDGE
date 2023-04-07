e9dc302206f2bd5acf66db115b0d2aa1169915ae
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-04-07 15:36:13 +0900

    백준 Lobby

---
+++ BackJoon/Bronze/B3/BB3_lobby.js
@@ -0,0 +1,15 @@
+// 2023-04-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input.forEach(v => {
+    if (v == "0 0 0") return;
+    let temp = v.split(" ").map(val => Number(val));
+    if (temp[0] / 2 <= temp[2]) answer.push(-1);
+    else if (temp[0] / 2 <= temp[1]) answer.push(0);
+    else {
+        answer.push(Math.floor(temp[0] / 2) + 1 - temp[1])
+    }
+})
+console.log(answer.join("\n"));
