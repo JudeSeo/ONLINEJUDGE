0727ba44f543e7aa72b0e9bc185d7eb3d8325f51
JudeSeo <gn0408@gmail.com>
2023-04-12 19:15:12 +0900

    백준 Word Searching

---
+++ BackJoon/Bronze/B3/BB3_wordSearching.js
@@ -0,0 +1,13 @@
+// 2023-04-12
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let word = input.splice(0, 1)[0];
+let answer = 0;
+input.forEach(v => {
+    while (v.indexOf(word) > -1) {
+        answer++;
+        v = v.slice(v.indexOf(word) + word.length);
+    }
+})
+console.log(answer);
