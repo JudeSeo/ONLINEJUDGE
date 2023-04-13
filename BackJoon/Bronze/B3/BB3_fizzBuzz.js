e22f35f53efe9e799443b7c79379b15066ac423a
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-04-13 11:41:21 +0900

    백준 FizzBuzz

---
+++ BackJoon/Bronze/B3/BB3_fizzBuzz.js
@@ -0,0 +1,16 @@
+// 2023-04-13
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v => Number(v));
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = [];
+for (let i = 1; i <=input[2] ; i++) {
+    if(i%input[0] == 0 && i%input[1] == 0)
+        answer.push("FizzBuzz");
+    else if(i%input[0] == 0)
+        answer.push("Fizz");
+    else if(i%input[1] == 0)
+        answer.push("Buzz");
+    else
+        answer.push(i)
+}
+console.log(answer.join("\n"));
