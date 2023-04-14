59859ab7aec8b7ca348299a1ba18190131c32c24
JudeSeo <gn0408@gmail.com>
2023-04-14 23:15:18 +0900

    백준 Making Change

---
+++ BackJoon/Bronze/B3/BB3_makingChange.js
@@ -0,0 +1,14 @@
+// 2023-04-14
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+let answer = [];
+let change = 100 - input;
+answer.push(parseInt(change / 25))
+change = change % 25;
+answer.push(parseInt(change / 10))
+change = change % 10;
+answer.push(parseInt(change / 5))
+change = change % 5;
+answer.push(change)
+console.log(answer.join(" "))
