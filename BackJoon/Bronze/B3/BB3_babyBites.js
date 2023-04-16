23554c7052003e515212cfab31bb5e24141a4c29
JudeSeo <gn0408@gmail.com>
2023-04-16 22:55:18 +0900

    백준 Baby Bites

---
+++ BackJoon/Bronze/B3/BB3_babyBites.js
@@ -0,0 +1,11 @@
+// 2023-04-16
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[1].split(" ");
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[1].split(" ");
+let cnt = 0;
+input.forEach((v, i) => {
+    if (v == "mumble") return;
+    else if (v != (i + 1)) cnt++;
+});
+let answer = cnt == 0 ? "makes sense" : "something is fishy";
+console.log(answer);
