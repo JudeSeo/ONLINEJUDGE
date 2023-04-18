aa8eda3e2f13d87a98d3570d7eb01d7dfdd10f83
JudeSeo <gn0408@gmail.com>
2023-04-18 20:43:12 +0900

    백준 Chronogram

---
+++ BackJoon/Bronze/B3/BB3_chronogram.js
@@ -0,0 +1,22 @@
+// 2023-04-18
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let chrono = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+};
+let answer = [];
+input.forEach(v => {
+    let sum = 0;
+    let temp = v.replaceAll(" ", "").split("").map(val => {
+        if (chrono[val] != undefined) sum += chrono[val];
+    });
+    answer.push(sum);
+})
+console.log(answer.join("\n"))
