864514411b55a4dfc2b487673910381e2a5c749e
JudeSeo <gn0408@gmail.com>
2023-04-19 22:24:33 +0900

    백준 Olympic Games

---
+++ BackJoon/Bronze/B3/BB3_olympicGames.js
@@ -0,0 +1,14 @@
+// 2023-04-19
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input.forEach(v => {
+    if (v == "0") return;
+    else if (v >= 1896 && v % 4 == 0) {
+        if ((v >= 1914 && v <= 1918) || (v > 1939 && v <= 1945)) answer.push(v + " Games cancelled");
+        else if (v > 2020) answer.push(v + " No city yet chosen");
+        else answer.push(v + " Summer Olympics");
+    } else answer.push(v + " No summer games");
+})
+console.log(answer.join("\n"))
