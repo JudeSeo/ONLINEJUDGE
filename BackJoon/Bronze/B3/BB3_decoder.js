41ca02d86a536a2218d26c72e4962543459b557d
JudeSeo <gn0408@gmail.com>
2023-04-22 19:51:23 +0900

    백준 Decoder

---
+++ BackJoon/Bronze/B3/BB3_decoder.js
@@ -0,0 +1,30 @@
+// 2023-04-22
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+let code = {
+    y: 'a',
+    u: 'y',
+    o: 'u',
+    i: 'o',
+    e: 'i',
+    a: 'e',
+    Y: 'A',
+    U: 'Y',
+    O: 'U',
+    I: 'O',
+    E: 'I',
+    A: 'E',
+}
+input.forEach(v => {
+    let temp = v.split("");
+    let tmp = [];
+    temp.forEach(val => {
+        if (code[val] != undefined)
+            val = code[val];
+        tmp.push(val)
+    })
+    answer.push(tmp.join(""))
+})
+console.log(answer.join("\n"));
