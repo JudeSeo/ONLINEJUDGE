ec478aec74959351819bb23895e179ba319264ba
JudeSeo <gn0408@gmail.com>
2023-04-24 22:28:29 +0900

    백준 Sarah's Toys

---
+++ BackJoon/Bronze/B3/BB3_Sarah'sToys.js
@@ -0,0 +1,14 @@
+// 2023-04-24
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');`
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input.forEach(v => {
+    if (v == "0 0") return;
+    let temp = v.split(" ").map(val => Number(val));
+    let doll = temp[0] - temp[1];
+    if (doll <= 1) answer.push("0 0");
+    else if (doll % 2 == 1) answer.push(`${Math.floor(doll / 2) - 1} 1`);
+    else answer.push(`${doll / 2} 0`)
+})
+console.log(answer.join("\n"))
