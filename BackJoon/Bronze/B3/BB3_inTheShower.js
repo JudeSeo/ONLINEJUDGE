574272edc51e3b8c283dd14114d2b2c007f05aad
JudeSeo <gn0408@gmail.com>
2023-04-30 23:31:14 +0900

    백준 In the Shower

---
+++ BackJoon/Bronze/B3/BB3_inTheShower.js
@@ -0,0 +1,13 @@
+// 2023-04-30
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+while (input.length > 0) {
+    let [e, n] = input.splice(0, 1)[0].split(" ").map(v => Number(v));
+    let temp = input.splice(0, n).map(v => Number(v));
+    let result = 0;
+    temp.map(v => v > e ? result++ : "");
+    answer.push(result)
+}
+console.log(answer.join("\n"))
