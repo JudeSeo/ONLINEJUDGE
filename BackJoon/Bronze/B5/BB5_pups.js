fc65aa92f9ca22f52172a30b2604dc6e8f193ba7
JudeSeo <gn0408@gmail.com>
2023-05-04 20:29:04 +0900

    백준 Pups

---
+++ BackJoon/Bronze/B5/BB5_pups.js
@@ -0,0 +1,10 @@
+// 2023-05-04
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach(v => {
+    let temp = v.split(" ").map(val => Number(val));
+    answer.push("$" + (temp[0] * temp[1] * temp[2]).toFixed(2));
+})
+console.log(answer.join("\n"))
