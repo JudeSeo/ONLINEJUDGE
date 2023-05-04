7b2d199f64c7db05619a3dfe442dad3932533ad6
JudeSeo <gn0408@gmail.com>
2023-05-04 20:23:23 +0900

    백준 직사각형

---
+++ BackJoon/Bronze/B5/BB5_rectangle.js
@@ -0,0 +1,6 @@
+// 2023-05-04
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer=input[0]*input[1];
+console.log(answer);
