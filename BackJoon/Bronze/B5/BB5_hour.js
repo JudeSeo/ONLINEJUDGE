4ffcb4c18001b3f0e8e3a26be22e2477d57f9401
JudeSeo <gn0408@gmail.com>
2023-05-14 21:25:29 +0900

    백준 時間 (Hour)

---
+++ BackJoon/Bronze/B5/BB5_hour.js
@@ -0,0 +1,6 @@
+// 2023-05-14
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+let answer=24;
+console.log(answer*input)
