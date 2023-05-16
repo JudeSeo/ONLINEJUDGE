cf1004e7c24a0f75c0aec0642e3f84ea589114b9
JudeSeo <gn0408@gmail.com>
2023-05-16 21:38:59 +0900

    백준 Gum Gum for Jay Jay

---
+++ BackJoon/Bronze/B5/BB5_gumGumForJayJay.js
@@ -0,0 +1,5 @@
+// 2023-05-16
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+console.log(input.length);
