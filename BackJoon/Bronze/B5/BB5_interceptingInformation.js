99384a8b1bff2dc305df966bfdf42b9e24ba709c
JudeSeo <gn0408@gmail.com>
2023-05-18 22:31:12 +0900

    백준 Intercepting Information

---
+++ BackJoon/Bronze/B5/BB5_interceptingInformation.js
@@ -0,0 +1,6 @@
+// 2023-05-18
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = input.filter(v => v == 9)
+console.log(answer.length > 0 ? "F" : "S");
