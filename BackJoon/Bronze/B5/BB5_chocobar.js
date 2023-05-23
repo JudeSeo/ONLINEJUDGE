64961ee5e75838f1b9263540e8e76544d9ce57e0
JudeSeo <gn0408@gmail.com>
2023-05-23 19:16:01 +0900

    백준 초코바

---
+++ BackJoon/Bronze/B5/BB5_chocobar.js
@@ -0,0 +1,6 @@
+// 2023-05-23
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = input[0] * 100 >= input[1] ? "Yes" : "No";
+console.log(answer);
