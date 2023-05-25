dc182fddb181dde266bb20aaad3c24735eac80f2
JudeSeo <gn0408@gmail.com>
2023-05-25 20:44:32 +0900

    백준 Робинзон Крузо

---
+++ BackJoon/Bronze/B5/BB5_pобинзон Крузо.js
@@ -0,0 +1,6 @@
+// 2023-05-25
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+let answer = "V".repeat(Math.floor(input / 5)) + "I".repeat(input % 5);
+console.log(answer);
