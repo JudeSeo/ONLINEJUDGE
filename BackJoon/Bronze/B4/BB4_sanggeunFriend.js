94e1b10d8b9960156d9608330e89c0877b3546df
JudeSeo <gn0408@gmail.com>
2023-06-02 23:32:26 +0900

    백준 상근이의 친구들

---
+++ BackJoon/Bronze/B4/BB4_sanggeunFriend.js
@@ -0,0 +1,11 @@
+// 2023-06-02
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input.forEach(v=>{
+    if(v=="0 0") return;
+    let temp = v.split(" ").map(val=>Number(val))
+    answer.push(temp[0]+temp[1])
+});
+console.log(answer.join("\n"))
