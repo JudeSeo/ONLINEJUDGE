b4f839837eeabf640f2d9ac63f76e4eb8dbcf4ec
JudeSeo <gn0408@gmail.com>
2023-06-05 22:27:44 +0900

    백준 Dedupe

---
+++ BackJoon/Bronze/B4/BB4_dedupe.js
@@ -0,0 +1,14 @@
+// 2023-06-05
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach(v => {
+    let temp = v.split("");
+    let tmp = [temp[0]];
+    for (let i = 1; i <= temp.length - 1; i++) {
+        if (tmp[tmp.length - 1] != temp[i]) tmp.push(temp[i]);
+    }
+    answer.push(tmp.join(""))
+})
+console.log(answer.join("\n"))
