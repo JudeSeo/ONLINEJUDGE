deecec29d8228a6331ded8e8d636cd2289777ab8
JudeSeo <gn0408@gmail.com>
2023-06-07 22:56:48 +0900

    백준 I Speak TXTMSG

---
+++ BackJoon/Bronze/B4/BB4_iSpeakTxtMsg.js
@@ -0,0 +1,23 @@
+// 2023-06-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+const shortForm = {
+    "CU": "see you",
+    ":-)": "I’m happy",
+    ":-(": "I’m unhappy",
+    ";-)": "wink",
+    ":-": "P	stick out my tongue",
+    "(~.~)": "sleepy",
+    "TA": "totally awesome",
+    "CCC": "Canadian Computing Competition",
+    "CUZ": "because",
+    "TY": "thank-you",
+    "YW": "you’re welcome",
+    "TTYL": "talk to you later",
+}
+input.forEach(v => {
+    answer.push(shortForm[v] ?? v);
+})
+console.log(answer.join("\n"))
