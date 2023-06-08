340d53f0e6b30334566bbd8388b7961b4af06f7b
JudeSeo <gn0408@gmail.com>
2023-06-08 22:54:43 +0900

    백준 Terms of Office

---
+++ BackJoon/Bronze/B4/BB4_termsOfOffice.js
@@ -0,0 +1,9 @@
+// 2023-06-08
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v => Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').map(v => Number(v));
+let answer = [];
+for (let i = input[0]; i <= input[1]; i += 60) {
+    answer.push(`All positions change in year ${i}`)
+}
+console.log(answer.join("\n"))
