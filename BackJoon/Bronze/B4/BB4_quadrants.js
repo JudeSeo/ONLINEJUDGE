bf508ee6a449aae7c64cd6dc77d9624b455ef2bf
JudeSeo <gn0408@gmail.com>
2023-06-14 23:20:32 +0900

    백준 Quadrants

---
+++ BackJoon/Bronze/B4/BB4_quadrants.js
@@ -0,0 +1,17 @@
+// 2023-06-14
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input.forEach(v => {
+    let temp = v.split(" ").map(val => Number(val));
+    if (temp[0] == 0 || temp[1] == 0) answer.push("AXIS");
+    else if (temp[0] > 0) {
+        if(temp[1]>0)answer.push("Q1");
+        else answer.push("Q4");
+    } else if (temp[0] < 0) {
+        if(temp[1]>0)answer.push("Q2");
+        else answer.push("Q3");
+    }
+})
+console.log(answer.join("\n"))
