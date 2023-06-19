7cd404659f6297fc2e7aaac1e56c3553432b984a
JudeSeo <gn0408@gmail.com>
2023-06-19 22:57:47 +0900

    백준 Electric Bill

---
+++ BackJoon/Bronze/B4/BB4_electricBill.js
@@ -0,0 +1,13 @@
+// 2023-06-19
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let [a, b] = input.splice(0, 2)[0].split(" ").map(v => Number(v));
+let answer = [];
+input.forEach(v => {
+    if (v >= 1000)
+        answer.push(`${v} ${1000 * a + (v - 1000) * b}`);
+    else
+        answer.push(`${v} ${v * a}`);
+})
+console.log(answer.join("\n"));
