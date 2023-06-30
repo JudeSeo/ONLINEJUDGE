fb1e79ed2df93eb7a82fcd184d114abd2ecbafcc
JudeSeo <gn0408@gmail.com>
2023-06-30 22:29:12 +0900

    백준 Reverse

---
+++ BackJoon/Bronze/B4/BB4_reverse.js
@@ -0,0 +1,12 @@
+// 2023-06-30
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach(v => {
+    let temp = v.split(" ");
+    let word = temp[0].split("")
+    let g = word.splice(temp[1], temp[2] - temp[1]);
+    answer.push(word.join(""))
+})
+console.log(answer.join("\n"))
