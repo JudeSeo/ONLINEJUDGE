56b17b3c40588e43187bad36a838b49c1bd5bcde
JudeSeo <gn0408@gmail.com>
2023-07-01 22:24:16 +0900

    백준 Zagubiona litera

---
+++ BackJoon/Bronze/B4/BB4_zagubionaLlitera.js
@@ -0,0 +1,10 @@
+// 2023-07-01
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+const alph = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let answer = "";
+alph.forEach(v => {
+    if (input.indexOf(v) == -1) answer = v;
+})
+console.log(answer);
