1ee9b5aeb40cb020802e1a0e4375a9718663a3e5
JudeSeo <gn0408@gmail.com>
2023-07-03 22:11:48 +0900

    백준 선물

---
+++ BackJoon/Bronze/B4/BB4_present.js
@@ -0,0 +1,11 @@
+// 2023-07-03
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+let [num, socks] = input.splice(0, 1)[0].split(" ").map(v => Number(v));
+input = input[0].split(" ").map(v => Number(v));
+for (let i = 0; i < num - 1; i++) {
+    answer.push(input[i] * socks + input[i + 1] * socks)
+}
+console.log(Math.min(...answer));
