408efcd18b94f3a3c7f83cfd593d9ee2321174da
JudeSeo <gn0408@gmail.com>
2023-07-05 19:12:57 +0900

    백준 Buying in Bulk

---
+++ BackJoon/Bronze/B4/BB4_buyingInBulk.js
@@ -0,0 +1,12 @@
+// 2023-07-05
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach(v => {
+    answer.push(v);
+    let temp = v.split(" ").map(val => Number(val));
+    if (temp[0] == 1) answer.push(temp[0] * temp[1]);
+    else answer.push(temp[0]*temp[1]-((temp[0] - 1) * 2))
+})
+console.log(answer.join("\n"));
