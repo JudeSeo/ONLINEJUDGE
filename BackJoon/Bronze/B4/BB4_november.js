e1eddb6d166c08ccb493a910de22e396e07074b8
JudeSeo <gn0408@gmail.com>
2023-07-06 20:23:10 +0900

    백준 11 月 (November)

---
+++ BackJoon/Bronze/B4/BB4_november.js
@@ -0,0 +1,7 @@
+// 2023-07-06
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v => Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').map(v => Number(v));
+let answer = 0;
+if (input[0] + input[1] * 7 <= 30) answer = 1;
+console.log(answer);
