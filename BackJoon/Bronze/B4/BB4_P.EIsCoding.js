9a421e57a52ad273d152453ff622a2f26bc48a4b
JudeSeo <gn0408@gmail.com>
2023-07-08 21:13:27 +0900

    백준 체육은 코딩과목 입니다

---
+++ BackJoon/Bronze/B4/BB4_P.EIsCoding.js
@@ -0,0 +1,22 @@
+// 2023-07-08
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = 1;
+let news = {1: "N", 2: "E", 3: "S", 4: "W"}
+input.forEach(v => {
+    if (v == 1) {
+        answer++
+        if (answer > 4) answer -= 4;
+        if (answer < 1) answer += 4;
+    } else if (v == 2) {
+        answer += 2
+        if (answer > 4) answer -= 4;
+        if (answer < 1) answer += 4;
+    } else if (v == 3) {
+        answer--
+        if (answer > 4) answer -= 4;
+        if (answer < 1) answer += 4;
+    }
+})
+console.log(news[answer]);
