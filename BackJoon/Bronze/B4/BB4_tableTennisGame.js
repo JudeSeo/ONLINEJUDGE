f518fef2e3d99ab208d925902aa7274f3bd73d33
JudeSeo <gn0408@gmail.com>
2023-07-09 14:00:54 +0900

    백준 탁구 경기

---
+++ BackJoon/Bronze/B4/BB4_tableTennisGame.js
@@ -0,0 +1,12 @@
+// 2023-07-09
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let d = 0;
+let p = 0;
+for (let i = 0; i < input.length; i++) {
+    if (input[i] == "D") d++;
+    if (input[i] == "P") p++
+    if (Math.abs(d - p) >= 2) break;
+}
+console.log(`${d}:${p}`)
