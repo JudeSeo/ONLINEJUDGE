d88938174ef4e5e7ca344792785d2893ac7322de
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-07-19 13:42:53 +0900

    Create BB4_mobis.js
    백준 모비스

---
+++ BackJoon/Bronze/B4/BB4_mobis.js
@@ -0,0 +1,9 @@
+// 2023-07-19
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split("");
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n')[0].split("");
+let answer = "NO";
+input.forEach(v => {
+    if (input.indexOf("M") > -1 && input.indexOf("O") > -1 && input.indexOf("B") > -1 && input.indexOf("I") > -1 && input.indexOf("S") > -1) answer = "YES"
+})
+console.log(answer);
