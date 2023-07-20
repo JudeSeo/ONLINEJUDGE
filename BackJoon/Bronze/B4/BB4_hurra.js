596c6f19f8bec8cd8825c5ff779ac0944976b4c1
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-07-20 11:22:04 +0900

    Create BB4_hurra.js
    백준 Hurra!

---
+++ BackJoon/Bronze/B4/BB4_hurra.js
@@ -0,0 +1,12 @@
+// 2023-07-20
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n')[0];
+let answer = []
+for (let i = 1; i <= Number(input); i++) {
+    if (i % 7 == 0 && i % 11 == 0) answer.push("Wiwat!");
+    else if (i % 7 == 0) answer.push("Hurra!")
+    else if (i % 11 == 0) answer.push("Super!");
+    else answer.push(i)
+}
+console.log(answer.join("\n"));
