ae3a5c2c432f1f52d1824b39c6114f322cef2857
JudeSeo <gn0408@gmail.com>
2023-08-13 21:48:07 +0900

    백준 Cornhole

---
+++ BackJoon/Bronze/B4/BB4_cornhole.js
@@ -0,0 +1,13 @@
+// 2023-08-13
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+const a = input[0].split(" ").map(v => Number(v));
+const b = input[1].split(" ").map(v => Number(v));
+const aScore = a[0] * 3 + a[1];
+const bScore = b[0] * 3 + b[1];
+let answer = '';
+if (aScore > bScore) answer = "1 " + (aScore - bScore);
+else if (aScore < bScore) answer = "2 " + (bScore - aScore);
+else answer = "NO SCORE";
+console.log(answer)
