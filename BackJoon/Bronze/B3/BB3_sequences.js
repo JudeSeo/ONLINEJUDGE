4c0b003c623ab4b373d2be17eb6b9a69b2763edf
JudeSeo <gn0408@gmail.com>
2023-08-21 23:19:43 +0900

    백준 Sequences

---
+++ BackJoon/Bronze/B3/BB3_sequences.js
@@ -0,0 +1,15 @@
+// 2023-08-21
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input.forEach(v => {
+    if (v == "0 0 0") return;
+    let temp = v.split(" ").map(val => Number(val));
+    let ans = temp[2] - temp[0] + temp[1]
+    if (ans % temp[1] || (temp[1] && temp[0] > temp[2]))
+        return answer.push("X");
+    else
+        return answer.push(Math.floor(ans / temp[1]))
+})
+console.log(answer.join("\n"))
