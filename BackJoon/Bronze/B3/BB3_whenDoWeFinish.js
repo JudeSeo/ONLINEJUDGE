42168880fd38cc709f21d49b7dc8e7bc52882f30
JudeSeo <gn0408@gmail.com>
2023-08-23 22:19:17 +0900

    백준 When Do We Finish?

---
+++ BackJoon/Bronze/B3/BB3_whenDoWeFinish.js
@@ -0,0 +1,25 @@
+// 2023-08-23
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input.forEach(v => {
+    if (v == "00:00 00:00") return;
+    let time = v.split(" ");
+    let start = time[0].split(":").map(v => Number(v));
+    let end = time[1].split(":").map(v => Number(v));
+    let min = start[1] + end[1];
+    let hour = start[0] + end[0]
+    let day = 0;
+    while (min >= 60) {
+        min -= 60;
+        hour += 1;
+    }
+    while (hour >= 24) {
+        hour -= 24
+        day++;
+    }
+    if (day > 0) answer.push(`${hour.toString().padStart(2,'0')}:${min.toString().padStart(2,'0')} +${day}`)
+    else answer.push(`${hour.toString().padStart(2,'0')}:${min.toString().padStart(2,'0')}`)
+})
+console.log(answer.join("\n"))
