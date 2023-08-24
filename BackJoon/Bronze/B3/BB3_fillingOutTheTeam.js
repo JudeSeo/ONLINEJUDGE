703ebb8a69e29bb7c5bc2cf2df70d751d9d713fb
JudeSeo <gn0408@gmail.com>
2023-08-24 19:40:19 +0900

    백준 Filling Out the Team

---
+++ BackJoon/Bronze/B3/BB3_fillingOutTheTeam.js
@@ -0,0 +1,19 @@
+// 2023-08-24
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input.forEach(v => {
+    if (v == "0 0 0") return;
+    let temp = v.split(" ");
+    let str = [];
+    if (temp[0] <= 4.5 && temp[1] >= 150 && temp[2] >= 200)
+        str.push("Wide Receiver");
+    if (temp[0] <= 6.0 && temp[1] >= 300 && temp[2] >= 500)
+        str.push("Lineman");
+    if (temp[0] <= 5.0 && temp[1] >= 200 && temp[2] >= 300)
+        str.push("Quarterback");
+    if (str == '') str.push("No positions")
+    answer.push(str.join(" "))
+})
+console.log(answer.join("\n"))
