550c89904be4afd5fb91dd84046d35a4bf087666
JudeSeo <gn0408@gmail.com>
2023-08-25 22:19:37 +0900

    백준 Chili Peppers

---
+++ BackJoon/Bronze/B4/BB4_chiliPeppers.js
@@ -0,0 +1,17 @@
+// 2023-08-25
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+const pepper = {
+    Poblano: 1500,
+    Mirasol: 6000,
+    Serrano: 15500,
+    Cayenne: 40000,
+    Thai: 75000,
+    Habanero: 125000,
+}
+let answer = 0;
+input.forEach(v => {
+    answer += pepper[v]
+})
+console.log(answer)
