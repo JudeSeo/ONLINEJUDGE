d9af278b874fade8869171b0278bfdd20365b37b
JudeSeo <gn0408@gmail.com>
2023-08-27 22:24:48 +0900

    백준 Counting Swann’s Coins

---
+++ BackJoon/Bronze/B3/BB3_countingSwann’sCoins.js
@@ -0,0 +1,12 @@
+// 2023-08-27
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+let answer = [];
+for (let i = 1; i <= input; i++) {
+    if (i % 3 == 0 && i % 5 == 0) answer.push("DeadMan\n");
+    else if (i % 3 == 0) answer.push("Dead\n");
+    else if (i % 5 == 0) answer.push("Man\n");
+    else answer.push(i+" ");
+}
+console.log(answer.join(""));
