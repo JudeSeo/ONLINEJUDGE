7fcae744c6d18dc9166b3ef513cd3262999e75fc
JudeSeo <gn0408@gmail.com>
2023-08-29 13:59:54 +0900

    백준 Adding Commas

---
+++ BackJoon/Bronze/B3/BB3_addingCommas.js
@@ -0,0 +1,18 @@
+// 2023-08-29
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+let regx = new RegExp(/(-?\d+)(\d{3})/);
+let bExists = input.indexOf(".", 0);
+let strArr = input.split('.');
+
+while (regx.test(strArr[0])) {
+    strArr[0] = strArr[0].replace(regx, "$1,$2");
+}
+
+if (bExists > -1) {
+    input = strArr[0] + "." + strArr[1];
+} else {
+    input = strArr[0];
+}
+console.log(input);
