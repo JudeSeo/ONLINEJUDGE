fc78811185504335ba58e83a3f382a0d87352ccc
JudeSeo <77234890+JudeSeo@users.noreply.github.com>
2023-08-30 15:07:31 +0900

    백준 Focus

---
+++ BackJoon/Bronze/B3/BB3_focus.js
@@ -0,0 +1,11 @@
+// 2023-08-30
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach(v => {
+    let temp = v.split(" ").map(val => Number(val));
+    let f = 1 / ((1 / temp[0]) + (1 / temp[1]))
+    answer.push("f = "+(Math.round(f*10)/10).toFixed(1));
+})
+console.log(answer.join("\n"));
