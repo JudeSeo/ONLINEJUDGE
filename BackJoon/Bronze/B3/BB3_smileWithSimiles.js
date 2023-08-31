d2aa1c27587399d6f31ad87c9633e195c512e4b1
JudeSeo <gn0408@gmail.com>
2023-08-31 19:08:28 +0900

    백준 Smile with Similes

---
+++ BackJoon/Bronze/B3/BB3_smileWithSimiles.js
@@ -0,0 +1,14 @@
+// 2023-08-31
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+let [adj, noun, ...words] = input;
+let ad = words.splice(0, adj);
+let no = words.splice(0, noun);
+ad.forEach(v=>{
+    no.forEach(val=>{
+        answer.push(`${v} as ${val}`);
+    })
+})
+console.log(answer.join("\n"));
