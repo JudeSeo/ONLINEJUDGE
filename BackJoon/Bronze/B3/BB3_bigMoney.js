c10926cd4ab07f0315a3de7b1f16252540ebd8f9
JudeSeo <gn0408@gmail.com>
2023-09-03 21:40:19 +0900

    백준 Big Money

---
+++ BackJoon/Bronze/B3/BB3_bigMoney.js
@@ -0,0 +1,11 @@
+// 2023-09-03
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v => Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').map(v => Number(v));
+let answer = 0;
+input.forEach(v => {
+    answer += v;
+})
+console.log(answer.toFixed(2))
+
+// 왜 틀렸는지 모르겠다....
