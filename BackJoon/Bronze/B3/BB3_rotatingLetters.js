ac5b2122c2105e2f1d082cab5589a28ec4ae0c50
JudeSeo <gn0408@gmail.com>
2023-09-08 22:30:03 +0900

    백준 Rotating letters

---
+++ BackJoon/Bronze/B3/BB3_rotatingLetters.js
@@ -0,0 +1,12 @@
+// 2023-09-08
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split("");
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split("");
+const letter = ["I", "O", "S", "H", "Z", "X", "N"]
+let flag = true
+input.forEach(v => {
+    if (!letter.includes(v))
+        flag = false;
+})
+if (flag) console.log("YES");
+else console.log("NO");
