4f61ba4d9418a3441dd40007897a2db195b90b17
JudeSeo <gn0408@gmail.com>
2023-09-13 22:23:45 +0900

    백준 3 つの箱 (Three Boxes)

---
+++ BackJoon/Bronze/B3/BB3_threeBoxes.js
@@ -0,0 +1,12 @@
+// 2023-09-13
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[1].split("");
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[1].split("");
+let answer = 0;
+let box = 1;
+input.forEach(v => {
+    if (v == "L") box = Math.max(1, box - 1);
+    else if (v == "R") box = Math.min(3, box + 1);
+    if (box == 3) answer++;
+})
+console.log(answer);
