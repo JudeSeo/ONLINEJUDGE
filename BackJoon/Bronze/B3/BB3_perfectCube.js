fa6ca6e70e48d08181a2d7ed9e3af11bcc57a927
JudeSeo <gn0408@gmail.com>
2023-09-16 23:21:15 +0900

    백준 Perfect Cube

---
+++ BackJoon/Bronze/B3/BB3_perfectCube.js
@@ -0,0 +1,10 @@
+// 2023-09-16
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach((v, i) => {
+    let temp = v.split(" ").map(val => Number(val));
+    answer.push(`Case #${i + 1}: ${Math.ceil(Math.cbrt(temp[1])) - Math.ceil(Math.cbrt(temp[0]))}`);
+})
+console.log(answer.join("\n"));
