4a99e54f9bd4acffb0a588b91661b1229711e5b7
JudeSeo <gn0408@gmail.com>
2023-09-17 21:11:14 +0900

    백준 Sumac Sequences

---
+++ BackJoon/Bronze/B3/BB3_sumacSequences.js
@@ -0,0 +1,14 @@
+// 2023-09-17
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v => Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').map(v => Number(v));
+let answer = 2;
+let num = 0;
+while (true) {
+    if (input[0] < input[1]) break;
+    num = input[0] - input[1]
+    input[0] = input[1];
+    input[1] = num;
+    answer++;
+}
+console.log(answer);
