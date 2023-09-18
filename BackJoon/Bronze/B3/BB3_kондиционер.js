56071c09d7db3978b44465e026e70dece2f78570
JudeSeo <gn0408@gmail.com>
2023-09-18 21:15:44 +0900

    백준 Кондиционер

---
+++ BackJoon/Bronze/B3/BB3_kондиционер.js
@@ -0,0 +1,21 @@
+// 2023-09-18
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = 0;
+let temp = input[0].split(" ")
+switch (input[1]) {
+    case "heat":
+        answer = Math.max(temp[0], temp[1])
+        break;
+    case "freeze":
+        answer = Math.min(temp[0], temp[1])
+        break;
+    case "auto":
+        answer = temp[1]
+        break;
+    case "fan":
+        answer = temp[0]
+        break;
+}
+console.log(answer);
