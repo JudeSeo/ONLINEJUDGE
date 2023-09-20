2efb62a48dd2cd8da6d752f8524642287e16bda6
JudeSeo <gn0408@gmail.com>
2023-09-20 22:05:24 +0900

    백준 Katmandu

---
+++ BackJoon/Bronze/B3/BB3_katmandu.js
@@ -0,0 +1,17 @@
+// 2023-09-20
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let [t, d, m] = input.splice(0, 1)[0].split(" ").map(v => Number(v));
+let answer = "Y"
+input.push(d);
+if (input[0] < t) {
+    let flag = false;
+    for (let i = 0; i < input.length - 1; i++) {
+        if (input[i + 1] - input[i] >= t) flag = true;
+    }
+    if (!flag) {
+        answer = "N";
+    }
+}
+console.log(answer);
