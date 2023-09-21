d328caa5ccd9bfa436e744007ae0a3c464fa3d24
JudeSeo <gn0408@gmail.com>
2023-09-21 21:08:35 +0900

    백준 Piłeczka

---
+++ BackJoon/Bronze/B3/BB3_piłeczka.js
@@ -0,0 +1,15 @@
+// 2023-09-21
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach(v => {
+    let temp = v.split(" ").map(val => BigInt(val));
+    let num = 0;
+    while (temp[0] < temp[1]) {
+        temp[0] = temp[0] * 2n;
+        num++;
+    }
+    answer.push(num);
+})
+console.log(answer.join("\n"));
