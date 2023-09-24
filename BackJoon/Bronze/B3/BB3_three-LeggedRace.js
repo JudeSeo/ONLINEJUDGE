813a636ee75323140983b532c5aac3e5cb6544b1
JudeSeo <gn0408@gmail.com>
2023-09-24 19:21:08 +0900

    백준 二人三脚 (Three-Legged Race)

---
+++ BackJoon/Bronze/B3/BB3_three-LeggedRace.js
@@ -0,0 +1,16 @@
+// 2023-09-24
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = [];
+input.forEach(v => {
+    if (answer.includes(v)) {
+        answer.splice(answer.indexOf(v), 1);
+    } else {
+        answer.push(v);
+    }
+})
+console.log(answer[0]);
+
+
+// 맞는거 같은데 왜 틀렸을까.....
