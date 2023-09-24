0b346c85def269192c250a1e8febc915e09fa47d
JudeSeo <gn0408@gmail.com>
2023-09-24 19:21:24 +0900

    미완료파일

---
+++ BackJoon/Bronze/B3/BB3_presidentialElection.js
@@ -0,0 +1,20 @@
+// 2022-12-01
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = 0;
+input = input.slice(1);
+let a = 0, b = 0;
+input.forEach(v => {
+    let temp = v.split(" ").map(val => Number(val));
+    a += temp[1]
+    b += temp[2]
+    if (temp[1] > temp[2])
+        a += temp[0]
+    else if (temp[1] < temp[2])
+        b += temp[0]
+
+})
+if (a > b) answer = 1;
+else if (a < b) answer = 2;
+console.log(answer)
---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello implements {
+    public static int a=100;
+    public static void main(String[] args){
+        public int b=10;
+        final double pi=3.14;
+
+    }
+}
