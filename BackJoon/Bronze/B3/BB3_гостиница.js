05a4cdcf7079adbb6210bb1f0df4424bf9c7fd77
JudeSeo <gn0408@gmail.com>
2023-10-08 17:32:01 +0900

    백준 Гостиница

---
+++ BackJoon/Bronze/B3/BB3_гостиница.js
@@ -0,0 +1,13 @@
+// 2023-10-08
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+let answer = [];
+if (input % 3 == 1) {
+    answer.push(2);
+    answer.push(Math.floor(input / 3) - 1)
+} else {
+    answer.push((input % 3) / 2)
+    answer.push(Math.floor(input / 3));
+}
+console.log(answer.join(" "));
