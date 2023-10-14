88f703fca5eecd9d2a20dd5b4df39da52c9ce8d2
JudeSeo <gn0408@gmail.com>
2023-10-14 22:28:40 +0900

    백준 체스 초보 브실이

---
+++ BackJoon/Bronze/B4/BB4_chessBeginner.js
@@ -0,0 +1,27 @@
+// 2023-10-14
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = 0;
+let chess = {
+    K: 0,
+    k: 0,
+    P: 1,
+    p: -1,
+    N: 3,
+    n: -3,
+    B: 3,
+    b: -3,
+    R: 5,
+    r: -5,
+    Q: 9,
+    q: -9
+}
+input.forEach(v => {
+    let temp = v.split("");
+    temp.forEach(val => {
+        if (val != '.')
+            answer += chess[val];
+    })
+})
+console.log(answer);
