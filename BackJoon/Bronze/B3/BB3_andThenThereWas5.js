26e6ecd3fbb1d8fcd4cebd1f60c1810ef8e287c0
JudeSeo <gn0408@gmail.com>
2023-10-16 20:05:07 +0900

    백준 And Then There Was 5

---
+++ BackJoon/Bronze/B3/BB3_andThenThereWas5.js
@@ -0,0 +1,10 @@
+// 2023-10-16
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach(v => {
+    let temp = v.split(" ");
+    answer.push(`${temp[1]} 5`);
+})
+console.log(answer.join("\n"));
