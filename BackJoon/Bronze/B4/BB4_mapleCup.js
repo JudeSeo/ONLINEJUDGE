d8e2907e04ce67b33e706fa3a0b51689420d3eb7
JudeSeo <gn0408@gmail.com>
2023-10-31 16:01:06 +0900

    백준 임스의 메이플컵

---
+++ BackJoon/Bronze/B4/BB4_mapleCup.js
@@ -0,0 +1,12 @@
+// 2023-10-31
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = "Bad";
+if(input[0] >= 1000){
+    answer = "Good";
+    if(input[1] >= 8000 || input[2] >=260){
+        answer = "Very Good";
+    }
+}
+console.log(answer);
