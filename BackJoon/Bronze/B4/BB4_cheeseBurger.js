05d976bc46d831ae84f519b53341afd5e5b6aec4
JudeSeo <gn0408@gmail.com>
2023-11-08 21:25:30 +0900

    백준 치즈버거 만들기

---
+++ BackJoon/Bronze/B4/BB4_cheeseBurger.js
@@ -0,0 +1,11 @@
+// 2023-11-08
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v=>Number(v));
+let answer = 0;
+if (input[0] > input[1]) {
+    answer = input[1] * 2 + 1;
+} else {
+    answer = input[0] * 2 - 1;
+}
+console.log(answer);
