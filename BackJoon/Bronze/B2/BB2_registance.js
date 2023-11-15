c28c43425d7311ca84859f0b8d5f1cfd0861fd11
JudeSeo <gn0408@gmail.com>
2023-11-15 21:23:46 +0900

    백준 저항

---
+++ BackJoon/Bronze/B2/BB2_registance.js
@@ -0,0 +1,30 @@
+// 2023-11-15
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let registance1 = {
+    black: "0",
+    brown: "1",
+    red: "2",
+    orange: "3",
+    yellow: "4",
+    green: "5",
+    blue: "6",
+    violet: "7",
+    grey: "8",
+    white: "9"
+}
+let registance2 = {
+    black: 1,
+    brown: 10,
+    red: 100,
+    orange: 1000,
+    yellow: 10000,
+    green: 100000,
+    blue: 1000000,
+    violet: 10000000,
+    grey: 100000000,
+    white: 1000000000
+}
+let answer = (registance1[input[0]] + registance1[input[1]]) * registance2[input[2]];
+console.log(answer);
