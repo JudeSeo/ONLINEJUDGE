4476c8e75ad599671338e09abf8c0f7fcb1a44db
JudeSeo <gn0408@gmail.com>
2023-11-20 19:43:03 +0900

    백준 이상한 곱셈

---
+++ BackJoon/Bronze/B2/BB2_strangeMultiple.js
@@ -0,0 +1,13 @@
+// 2023-11-20
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ");
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ");
+let answer = 0;
+let first = input[0].split("").map(v => Number(v));
+let second = input[1].split("").map(v => Number(v));
+first.forEach(v => {
+    second.forEach(val => {
+        answer += (v * val);
+    })
+})
+console.log(answer);
