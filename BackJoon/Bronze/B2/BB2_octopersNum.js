afcef32e8b1475787f1bdc921660be19f1ee98c2
JudeSeo <gn0408@gmail.com>
2023-11-26 21:25:30 +0900

    백준 문어 숫자

---
+++ BackJoon/Bronze/B2/BB2_octopersNum.js
@@ -0,0 +1,26 @@
+// 2023-11-26
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+const num = {
+    "-": 0,
+    "\\": 1,
+    "(": 2,
+    "@": 3,
+    "?": 4,
+    ">": 5,
+    "&": 6,
+    "%": 7,
+    "/": -1
+}
+let answer = [];
+input.forEach(v => {
+    if (v == "#") return;
+    let temp = v.split("").map(val => num[val]);
+    let sum = 0;
+    temp.forEach((val, index) => {
+        sum += (8 ** (temp.length - index-1) * val);
+    })
+    answer.push(sum);
+})
+console.log(answer.join("\n"));
