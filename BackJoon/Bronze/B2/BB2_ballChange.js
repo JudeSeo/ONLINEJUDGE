e7145d58596273b2ee6efa350bc4aa37a0a727ae
JudeSeo <gn0408@gmail.com>
2023-11-28 22:54:10 +0900

    백준 공 바꾸기

---
+++ BackJoon/Bronze/B2/BB2_ballChange.js
@@ -0,0 +1,16 @@
+// 2023-11-28
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let [n, m] = input.splice(0, 1)[0].split(" ").map(v => Number(v));
+let ball = new Array(n + 1).fill(0);
+for (let i = 0; i <= n; i++) {
+    ball[i] = i;
+}
+input.forEach(v => {
+    let temp = v.split(" ");
+    let tmp = ball[temp[0]];
+    ball[temp[0]] = ball[temp[1]];
+    ball[temp[1]] = tmp;
+})
+console.log(ball.slice(1).join(" "));
