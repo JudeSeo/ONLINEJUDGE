626667f09dbe5af57e6e401f6347223261da056b
JudeSeo <gn0408@gmail.com>
2023-12-07 20:39:15 +0900

    백준 Cryptoquote

---
+++ BackJoon/Bronze/B2/BB2_cryptoquote.js
@@ -0,0 +1,19 @@
+// 2023-12-07
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+while (input.length > 0) {
+    let str = input.splice(0, 1)[0].split("");
+    let enc = input.splice(0, 1)[0].split("");
+    let dec = ""
+    str.forEach(v => {
+        if (v == " ") dec += " ";
+        else {
+            let ascii = v.charCodeAt(0) - 65;
+            dec += enc[ascii];
+        }
+    })
+    answer.push(dec);
+}
+console.log(answer.join("\n"));
