4f5cc3ee79abc94b39d1ed3813e341f5c9310fb1
JudeSeo <gn0408@gmail.com>
2023-12-17 21:13:40 +0900

    백준 해밍 거리

---
+++ BackJoon/Bronze/B2/BB2_hammingDistance.js
@@ -0,0 +1,15 @@
+// 2023-12-17
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+while (input.length > 0) {
+    let temp1 = input.splice(0, 1)[0].split("");
+    let temp2 = input.splice(0, 1)[0].split("");
+    let sum = 0;
+    temp1.forEach((v, i) => {
+        if (v != temp2[i]) sum++;
+    })
+    answer.push(`Hamming distance is ${sum}.`);
+}
+console.log(answer.join("\n"));
