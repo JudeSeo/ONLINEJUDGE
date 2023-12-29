fed7f9df9f805772b11d220a778fd90748afa45a
JudeSeo <gn0408@gmail.com>
2023-12-29 19:37:13 +0900

    백준 문자열 분석

---
+++ BackJoon/Bronze/B2/BB2_stringAnalysis.js
@@ -0,0 +1,19 @@
+// 2023-12-29
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().split('\n');
+let answer = [];
+input.forEach(v => {
+    if (v == "") return;
+    v = v.split("");
+    let lower = 0, upper = 0, blank = 0, number = 0;
+    v.map(val => {
+        let asc = val.charCodeAt();
+        if (asc >= 65 && asc <= 90) upper++;
+        else if (asc >= 97 && asc <= 122) lower++;
+        else if (asc >= 48 && asc <= 57) number++;
+        else blank++;
+    })
+    answer.push(`${lower} ${upper} ${number} ${blank}`);
+})
+console.log(answer.join("\n"));
