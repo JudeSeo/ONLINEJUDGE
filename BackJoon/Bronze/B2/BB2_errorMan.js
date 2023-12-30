757c318e2eafdf51be91772c562889f112bfeb35
JudeSeo <gn0408@gmail.com>
2023-12-30 10:51:58 +0900

    백준 오타맨 고창영

---
+++ BackJoon/Bronze/B2/BB2_errorMan.js
@@ -0,0 +1,12 @@
+// 2023-12-30
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach(v => {
+    let temp = v.split(" ");
+    let str = temp[1].split("");
+    let error = str.splice(temp[0] - 1, 1)[0];
+    answer.push(str.join(""));
+})
+console.log(answer.join("\n"));
