02e98c7b64c05db104640657d174b41e6ba389ea
JudeSeo <gn0408@gmail.com>
2024-01-04 22:41:10 +0900

    백준 점수 집계

---
+++ BackJoon/Bronze/B2/BB2_scoreTotal.js
@@ -0,0 +1,11 @@
+// 2024-01-04
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach(v => {
+    let temp = v.split(" ").map(val => Number(val)).sort((a, b) => a - b);
+    if (temp[3] - temp[1] >= 4) answer.push("KIN");
+    else answer.push(temp[1] + temp[2] + temp[3]);
+})
+console.log(answer.join("\n"));
