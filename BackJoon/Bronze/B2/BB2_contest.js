1b34816e339a7d2a3370d5d1ed7a1a750ec83d6e
JudeSeo <gn0408@gmail.com>
2024-01-09 21:21:58 +0900

    백준 콘테스트

---
+++ BackJoon/Bronze/B2/BB2_contest.js
@@ -0,0 +1,11 @@
+// 2024-01-09
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+let w = input.splice(0, 10).map(v => Number(v)).sort((a, b) => b - a);
+let k = input.splice(0, 10).map(v => Number(v)).sort((a, b) => b - a);
+let wSum = w[0] + w[1] + w[2];
+let kSum = k[0] + k[1] + k[2];
+answer.push(`${wSum} ${kSum}`);
+console.log(answer.join(" "));
