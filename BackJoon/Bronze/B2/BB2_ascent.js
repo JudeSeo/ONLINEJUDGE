3cb3c2f88f7ed2683ad7d6bb64a297dcac38c837
JudeSeo <gn0408@gmail.com>
2024-01-12 20:05:33 +0900

    백준 오르막

---
+++ BackJoon/Bronze/B2/BB2_ascent.js
@@ -0,0 +1,8 @@
+// 2024-01-12
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v => Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = "Good";
+let temp = input.slice(0).sort((a, b) => a - b);
+if (JSON.stringify(input) != JSON.stringify(temp)) answer = "Bad";
+console.log(answer);
