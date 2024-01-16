2b8486b5191a7203e9edefda5c3a7ff600a8c559
JudeSeo <gn0408@gmail.com>
2024-01-16 20:19:21 +0900

    백준 동혁 피자

---
+++ BackJoon/Bronze/B2/BB2_pizza.js
@@ -0,0 +1,12 @@
+// 2024-01-16
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input.forEach((v, i) => {
+    if (v == "0") return;
+    let temp = v.split(" ");
+    if (temp[0] * 2 < Math.sqrt(temp[1] ** 2 + temp[2] ** 2)) answer.push(`Pizza ${i + 1} does not fit on the table.`);
+    else answer.push(`Pizza ${i + 1} fits on the table.`);
+})
+console.log(answer.join("\n"));
