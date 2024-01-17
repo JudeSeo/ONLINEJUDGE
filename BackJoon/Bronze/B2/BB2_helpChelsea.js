5c22a48c3fbb90f9c632b0182a565f69eb25e9c8
JudeSeo <gn0408@gmail.com>
2024-01-17 19:24:30 +0900

    백준 첼시를 도와줘!

---
+++ BackJoon/Bronze/B2/BB2_helpChelsea.js
@@ -0,0 +1,20 @@
+// 2024-01-17
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+while (input.length > 0) {
+    let num = input.splice(0, 1)[0];
+    let list = input.splice(0, num);
+    let maxPrice = 0;
+    let exPerson = "";
+    list.forEach(v => {
+        let temp = v.split(" ");
+        if (maxPrice < Number(temp[0])) {
+            maxPrice = Number(temp[0]);
+            exPerson = temp[1];
+        }
+    })
+    answer.push(exPerson);
+}
+console.log(answer.join("\n"));
