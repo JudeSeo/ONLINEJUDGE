0c02053968dd6b6c1a7db49f04621d09477f2098
JudeSeo <gn0408@gmail.com>
2024-01-18 21:48:27 +0900

    백준 연속구간

---
+++ BackJoon/Bronze/B2/BB2_continuousSection.js
@@ -0,0 +1,19 @@
+// 2024-01-18
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = [];
+input.forEach(v => {
+    let temp = v.split("");
+    let prevNum = "";
+    let cnt = 0;
+    let max = 0;
+    temp.forEach(val => {
+        if (val == prevNum) cnt++;
+        else cnt = 1;
+        prevNum = val;
+        max = Math.max(max, cnt);
+    })
+    answer.push(max);
+})
+console.log(answer.join("\n"));
