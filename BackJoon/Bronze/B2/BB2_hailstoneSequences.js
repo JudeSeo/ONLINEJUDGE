128cecf53580480fe9b995d2a548aaa1086cc8cf
JudeSeo <gn0408@gmail.com>
2024-01-23 20:56:44 +0900

    백준 헤일스톤 수열

---
+++ BackJoon/Bronze/B2/BB2_hailstoneSequences.js
@@ -0,0 +1,17 @@
+// 2024-01-23
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1).map(v=>Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1).map(v => Number(v));
+let answer = [];
+
+input.forEach(v => {
+    let num = v;
+    while (v != 1) {
+        if (v < (num / 10)) break;
+        if (v % 2 == 0) v /= 2;
+        else v = v * 3 + 1;
+        num = Math.max(num, v);
+    }
+    answer.push(num);
+})
+console.log(answer.join("\n"));
