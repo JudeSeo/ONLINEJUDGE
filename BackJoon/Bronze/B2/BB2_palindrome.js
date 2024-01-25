6c758ba61260d58db2a771a25a7aca178062ac2e
JudeSeo <gn0408@gmail.com>
2024-01-25 21:19:46 +0900

    백준 팰린드롬

---
+++ BackJoon/Bronze/B2/BB2_palindrome.js
@@ -0,0 +1,11 @@
+// 2024-01-25
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1).map(v => v.trim().toLowerCase());
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1).map(v => v.trim().toLowerCase());
+let answer = [];
+input.forEach(v => {
+    let temp = v.split("").reverse().join("");
+    if (v == temp) answer.push("Yes");
+    else answer.push("No");
+})
+console.log(answer.join("\n"));
