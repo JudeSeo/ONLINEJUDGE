9e5f190257666af6709b5c7e1ea6a7d275cb40a4
JudeSeo <gn0408@gmail.com>
2024-01-27 10:37:04 +0900

    백준 부분 문자열

---
+++ BackJoon/Bronze/B2/BB2_partString.js
@@ -0,0 +1,33 @@
+// 2024-01-27
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n');
+let answer = 0;
+const [s, p] = input;
+const sLen = s.length;
+const pLen = p.length;
+const check = new Array(sLen).fill(0);
+let j = 0;
+for (let i = 1; i < sLen; i++) {
+    while (j > 0 && s[i] !== s[j]) j = check[j - 1];
+    if (s[j] === s[i]) {
+        check[i] = j + 1;
+        j++;
+    } else j = 0;
+}
+
+j = 0;
+for (let i = 0; i < sLen; i++) {
+    while (j > 0 && s[i] !== p[j]) j = check[j - 1];
+    if (s[i] === p[j]) {
+        // console.log(i, j);
+        if (j === pLen - 1) {
+            answer = 1;
+            break;
+        } else j++;
+    }
+}
+console.log(answer);
+
+// KMP로 풀라고 하는데 모르겠다....
+// 다른사람꺼로 보는데 넘나 어렵...
