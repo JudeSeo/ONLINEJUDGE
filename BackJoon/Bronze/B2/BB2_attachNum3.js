87e97c38b8ed59f79ef80ddb13e39ce9aee5e789
JudeSeo <gn0408@gmail.com>
2024-01-29 20:59:04 +0900

    백준 수 이어 쓰기 3

---
+++ BackJoon/Bronze/B2/BB2_attachNum3.js
@@ -0,0 +1,9 @@
+// 2024-01-29
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+let str = "";
+for (let i = 1; i <= input; i++) {
+    str += i
+}
+console.log(str.indexOf(input)+1);
