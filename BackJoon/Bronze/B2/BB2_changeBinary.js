fb6cd19e1826886641c7b8ff715cc33c5a340719
JudeSeo <gn0408@gmail.com>
2024-02-08 20:21:15 +0900

    백준 이진수 변환

---
+++ BackJoon/Bronze/B2/BB2_changeBinary.js
@@ -0,0 +1,5 @@
+// 2024-02-08
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+let input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+console.log(BigInt(input).toString(2));
