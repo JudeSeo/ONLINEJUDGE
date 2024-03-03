7eed9304a429c3b35620c83b3481106fd63f2d1a
JudeSeo <gn0408@gmail.com>
2024-03-03 21:46:36 +0900

    백준 9진수

---
+++ BackJoon/Bronze/B2/BB2_9digitNum.js
@@ -0,0 +1,6 @@
+// 2024-03-03
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+let input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+let answer = Number(input).toString(9);
+console.log(answer);
