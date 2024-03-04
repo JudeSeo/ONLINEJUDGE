69b9bd8525d2473d752acd4b58491fe9c7fb92a1
JudeSeo <gn0408@gmail.com>
2024-03-04 21:57:13 +0900

    백준 요다

---
+++ BackJoon/Bronze/B2/BB2_yoda.js
@@ -0,0 +1,11 @@
+// 2024-03-04
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+let input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = [];
+input.forEach(v => {
+    let temp = v.split(" ");
+    temp.push(...temp.splice(0, 2));
+    answer.push(temp.join(" "));
+})
+console.log(answer.join("\n"));
