f63d0b1742adbd8b63c8f2214ca541efba1f991c
JudeSeo <gn0408@gmail.com>
2024-05-18 14:13:48 +0900

    백준 A번 SMUPC NAME

---
+++ BackJoon/Contest/smupcName.js
@@ -0,0 +1,8 @@
+// 2024-05-18
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ");
+let input = fs.readFileSync('../../예제.txt').toString().trim().split('\r\n')[0].split(" ");
+let name = Array.from(new Set(input[1].split("")));
+let length = Number(input[1].split("").length - name.length);
+let answer = "smupc_" + (((1906 + Number(input[0])) + name.join("") + (length+4)).split("").reverse().join(""))
+console.log(answer);
