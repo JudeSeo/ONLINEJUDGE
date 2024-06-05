08974ff597578a106ff8f4599e50218ab12ac1c5
HyeJu Seo <77234890+JudeSeo@users.noreply.github.com>
2024-06-05 16:30:31 +0900

    백준 K번째 수
    K번째 수

--- BackJoon/Silver/BS5_kthNum.js
+++ BackJoon/Silver/BS5_kthNum.js
@@ -1,12 +1,9 @@
-// 2022-03-07
+// 2024-06-05
 let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 let input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
-let answer = [];
-let kth = input[0].split(" ")[1];
-let nums = input[1].split(" ");
-nums.sort((a, b) => (a - b))
-console.log(nums[kth - 1]);
+let [n, k] = input[0].split(" ");
+let nums = input[1].split(" ").map(v => Number(v)).sort((a, b) => (a - b));
+console.log(nums[k - 1]);
 
-// 시간초과 떴다...
-// 이렇게 푸는게 맞는거 같은데..ㅜ
+// 정렬 전 숫자 타입으로 바꿔주니까 통과했다...
