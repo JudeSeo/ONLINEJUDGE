bab26e3dae1fa22a95d2bf9a70507b4129d98e01
HyeJu Seo <77234890+JudeSeo@users.noreply.github.com>
2024-06-10 09:09:48 +0900

    백준 세제곱의 합
    세제곱의 합

---
+++ BackJoon/Bronze/B5/BB5_cubesSum.js
@@ -0,0 +1,7 @@
+// 2024-06-10
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v))[0];
+var input = fs.readFileSync("예제2.txt").toString().trim().split('\r\n').map(v=>Number(v))[0];
+let sum=(input*(input+1))/2;
+let answer = [sum, sum**2, sum**2];
+console.log(answer.join("\n"));
