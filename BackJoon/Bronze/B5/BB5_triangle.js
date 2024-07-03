3f752dfc262833c3ff9fd9b9497b2f99b5c6df85
JudeSeo <gn0408@gmail.com>
2024-07-03 20:22:30 +0900

    백준 삼각형

---
+++ BackJoon/Bronze/B5/BB5_triangle.js
@@ -0,0 +1,6 @@
+// 2024-07-03
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = input[0] * input[1] / 2;
+console.log(answer.toFixed(1));
