a119d4a631b5a799fc5bb78876cd26803ed22486
JudeSeo <gn0408@gmail.com>
2024-07-22 19:49:12 +0900

    백준 Haughty Cuisine

---
+++ BackJoon/Bronze/B4/BB4_haughtyCuisine.js
@@ -0,0 +1,6 @@
+// 2024-07-22
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').slice(1);
+let answer = input[0].split(" ");
+console.log(answer.join("\n"));
