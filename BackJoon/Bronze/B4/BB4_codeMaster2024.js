aefe6600e88eb6859e9a1cdd09b6d43f9ba337f0
JudeSeo <gn0408@gmail.com>
2024-09-11 20:04:10 +0900

    백준 코드마스터 2024

---
+++ BackJoon/Bronze/B4/BB4_codeMaster2024.js
@@ -0,0 +1,6 @@
+// 2024-09-11
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0].split(" ").map(v=>Number(v));
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0].split(" ").map(v => Number(v));
+let answer = (input[2] + 1) * input[1];
+console.log(answer);
