a976dfabb41e452143bdbaa0346695ceb8c043d9
HyeJu Seo <77234890+JudeSeo@users.noreply.github.com>
2024-10-16 08:17:14 +0900

    백준 크로아티아 알파벳
    백준 크로아티아 알파벳

---
+++ BackJoon/Silver/BS5_croatianAlphabet.js
@@ -0,0 +1,7 @@
+// 2024-10-16
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("예제2.txt").toString().trim().split('\r\n')[0];
+input = input.replace(/c=|c-|dz=|d-|lj|nj|s=|z=/g , '1');
+let answer = input.length
+console.log(answer);
