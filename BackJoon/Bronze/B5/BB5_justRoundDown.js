c36ed75a7f14e4b3538266dc69fee3bb2423020e
JudeSeo <gn0408@gmail.com>
2024-12-10 21:18:49 +0900

    백준 Just Round Down

---
+++ BackJoon/Bronze/B5/BB5_justRoundDown.js
@@ -0,0 +1,6 @@
+// 2024-12-10
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+var input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+let answer = Math.floor(Number(input));
+console.log(answer);
