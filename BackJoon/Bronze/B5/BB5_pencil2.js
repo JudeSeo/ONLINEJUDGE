0c0b145b4614f9db5a2dfa8a56b045081eb069f0
HyeJu Seo <77234890+JudeSeo@users.noreply.github.com>
2025-03-06 10:32:08 +0900

    백준 鉛筆2

---
+++ BackJoon/Bronze/B5/BB5_pencil2.js
@@ -0,0 +1,6 @@
+// 2025-03-06
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var input = fs.readFileSync("예제2.txt").toString().trim().split('\r\n');
+let answer = Math.floor(input/5);
+console.log(answer);
