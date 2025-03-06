d4deb1d3c4036c04c7cf17b4dbf2684f37135ea1
HyeJu Seo <77234890+JudeSeo@users.noreply.github.com>
2025-03-06 10:54:45 +0900

    백준 가희와 4시간의 벽 1

---
+++ BackJoon/Bronze/B5/BB5_4hoursOfWall1.js
@@ -0,0 +1,6 @@
+// 2025-03-06
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
+var input = fs.readFileSync("예제2.txt").toString().trim().split('\r\n').map(v=>Number(v));
+let answer = input[0] > input[1] ? "flight" : "high speed rail";
+console.log(answer);
