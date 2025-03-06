f111fff5edab3eef97fcc4db6a5504857cfd2ddf
HyeJu Seo <77234890+JudeSeo@users.noreply.github.com>
2025-03-06 10:43:42 +0900

    백준 Call for Problems

---
+++ BackJoon/Bronze/B5/BB5_CallForProblems.js
@@ -0,0 +1,6 @@
+// 2025-03-06
+var fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1).map(v=>Number(v));
+var input = fs.readFileSync("예제2.txt").toString().trim().split('\r\n').slice(1).map(v=>Number(v));
+let answer = input.filter(v=>v%2==1).length;
+console.log(answer);
