b1bac487829ca9e628485e2bbbab8f9428f2a519
JudeSeo <gn0408@gmail.com>
2025-03-07 21:18:05 +0900

    백준 인간은 무엇인가

--- BackJoon/Bronze/B4/BB4_digit.js
+++ BackJoon/Bronze/B4/BB4_digit.js
@@ -1,6 +1,7 @@
 // 2025-03-07
 let fs = require('fs');
-// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(v=>Number(v));
-let input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n').map(v => Number(v));
-let answer = (input[0] + input[1]).toString().length;
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')[0];
+let input = fs.readFileSync("../../../예제.txt").toString().trim().split('\r\n')[0];
+let answer = input % 2024 == 0 ? "Yes" : "No";
+if (input > 100000) answer = "No";
 console.log(answer);
