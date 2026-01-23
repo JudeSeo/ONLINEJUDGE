6bfc3c58626ae6badbfaa5cba2d2d7b8f8a2c82d
서혜주 <hjseo@entropykorea.com>
2026-01-23 15:53:46 +0900

    프로그래머스 몫 구하기

---
+++ Programmers/level0/P0_quotient.java
@@ -0,0 +1,12 @@
+package Programmers.level0;
+
+public class P0_quotient {
+    public static void main(String[] args) {
+        System.out.println(solution(7, 2));
+    }
+
+    public static int solution(int num1, int num2) {
+        int answer = num1 / num2;
+        return answer;
+    }
+}
