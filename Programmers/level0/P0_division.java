a84ebff187b6bfbaef76a9ee7a3d875e9b310d28
서혜주 <hjseo@entropykorea.com>
2026-01-23 15:52:03 +0900

    프로그래머스 두 수의 나눗셈

--- Programmers/level0/P0_division.java
+++ Programmers/level0/P0_division.java
@@ -6,7 +6,7 @@ public class P0_division {
     }
 
     public static int solution(int num1, int num2) {
-        int answer = (int) Math.floor(num1 * 1000 / num2);
+        int answer = num1 * 1000 / num2;
         return answer;
     }
 }
