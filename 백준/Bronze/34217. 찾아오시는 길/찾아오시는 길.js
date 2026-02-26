e79e47a44807ed88254db876e2f246b10691b7c7
HyeJu Seo <77234890+JudeSeo@users.noreply.github.com>
2026-02-26 23:06:31 +0900

    [Bronze V] Title: 찾아오시는 길, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.js
@@ -0,0 +1,7 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// let input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
+let [a, b] = input[0].split(" ").map(v => Number(v));
+let [c, d] = input[1].split(" ").map(v => Number(v));
+let answer = a + c == b + d ? "Either" : a + c < b + d ? "Hanyang Univ." : "Yongdap";
+console.log(answer);
