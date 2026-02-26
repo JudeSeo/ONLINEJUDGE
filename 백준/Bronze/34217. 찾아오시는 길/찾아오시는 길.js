c7b3dd171fa2b451163b4d63731f42d303ca0c6c
JudeSeo <gn0408@gmail.com>
2026-02-26 23:09:09 +0900

    날짜 추가

--- 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.js
+++ 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.js
@@ -1,3 +1,4 @@
+// 2026-02-26
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 // let input = fs.readFileSync('예제.txt').toString().trim().split('\r\n');
